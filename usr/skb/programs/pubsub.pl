83f6e5d61205d5a8a74919af4d0ed952ca050a49
Gerd Zellweger <zgerd@student.ethz.ch>
2011-10-31 15:38:35 +0100

    publish subscribe find_subscribers working

--- usr/skb/programs/pubsub.pl
+++ usr/skb/programs/pubsub.pl
@@ -9,13 +9,12 @@
 subscribed(object(msg,  [ hair::[ val blonde ]]), [], me).
 subscribed(object(msg,  [ hair::_, attr::[val bla]]), [], me).
 subscribed(object(_,    [ face::[ val test ]]), [], me).
+subscribed(object(name, [ face::[ val red ], age::[ val 22 ]]), [], subs).
 subscribed(object(name, [ face::[ val red ], age::_ ] ), [ constraint(age, '>', 10) ], subs).
-%subscribed(object(_, [ age::_ ] ), [ constraint(age, '=<', 10) ], subs2).
-%subscribed(object(_, [ age::_ ] ), [ constraint(age, '>=', 13) ], subs3).
+subscribed(object(_, [ age::_ ] ), [ constraint(age, '=<', 10) ], subs2).
+subscribed(object(_, [ age::_ ] ), [ constraint(age, '>=', 13) ], subs3).
+subscribed(object(_, [ other::_, theval::_ ] ), [ constraint(theval, '>', 13) ], subs4).
 
-% FIXME: wrong order does not match
-subscribed(object(_, [ other::_, age::_ ] ), [ constraint(age, '>', 13) ], subs4).
-%
 
 find_subscriber(Message, Subscriber) :-
     find_subscription(Message, Constraints, Subscriber),
@@ -26,13 +25,25 @@ find_subscription(Message, Constraints, Subscriber) :-
     matches_template(Message, Template).
     
 matches_template(object(OName, OSlotList), object(TName, TSlotList)) :-
-    slot_vals(OName, Attr::X, OSlotList),
-    slot_vals(TName, Attr::X, TSlotList).
+    match_lists(TSlotList, OSlotList).
 
+match_lists([], _).
+match_lists([Attr::X|TRest], OList) :-
+    member(Attr::Y, OList),
+    (var(X)),
+    match_lists(TRest, OList).
+match_lists([Attr::X|TRest], OList) :-
+    member(Attr::Y, OList),
+    member(val(V1), X),
+    member(val(V2), Y),
+    !,
+    (V1 == V2),
+    match_lists(TRest, OList).
+
+satisfy_constraints(Message, []).
 satisfy_constraints(Message, [C|Rest]) :-
     satisfy_constraint(Message, C),
     satisfy_constraints(Message, Rest).
-
 satisfy_constraint(object(Name, SlotList), constraint(Attr, Comparator, Value)) :-
     %get_object(Message, Attr::X),
     slot_vals(Name, Attr::X, SlotList),
--- usr/skb/programs/pubsubtest.tst
+++ usr/skb/programs/pubsubtest.tst
@@ -4,16 +4,17 @@ subscribed(object(msg, [hair::[val blonde]]), _, X) should_give X=me.
 subscribed(object(msg, [hair::[val blabla], attr::[val bla] ]), _, X) should_give X=me.
 subscribed(object(na,  [face::[val test]]), _, X) should_give X=me.
 
+match_lists([face::[ val red ], age::[ val 22 ]], [face::[ val red ], age::[ val 22 ]]).
+match_lists([face::_, age::[ val 22 ]], [face::[ val red ], age::[ val 22 ]]).
+match_lists([face::_, age::[ val 21 ]], [face::[ val red ], age::[ val 22 ]]).
+match_lists([face::[ val red ], age::[ val 22 ]], [face::[ val red ], age::[ val 22 ]]).
+match_lists([ other::_, theval::_ ], [theval::[ val 14 ], other::[val 'name'] ]).
+
 find_subscriber(object(name, [face::[ val red ], age::[ val 22 ]]), X) should_give X=subs.
-
-/*
-
-find_subscriber(object(wrongname, [face::[ val red ], age::[ val 22 ]]), X) should_fail.
-find_subscriber(object(wrongname, [face::[ val red ], age::[ val 9 ]]), X) should_fail.
 find_subscriber(object(xx, [age::[ val 9 ]]), X) should_give X=subs2.
 find_subscriber(object(xx, [age::[ val 13 ]]), X) should_give X=subs3.
-find_subscriber(object(xx, [age::[ val 14 ], other::[val 'name'] ]), X) should_give X=subs4.
-*/
+find_subscriber(object(xx, [theval::[ val 14 ], other::[val 'name'] ]), X) should_give X=subs4.
+
 
 % Using constraints
 %subscribed(object(_, [weight::20]), other).
