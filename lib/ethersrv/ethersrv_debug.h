7dbfd9102c09b3e344e2f12c1d9f7608296bcc85
pravin shindep@inf.ethz.ch <pravin shindep@inf.ethz.ch>
2011-11-13 19:12:03 +0100

    Refactoring of code: Added ethersrv_ctl.c to separate out the code dealing with ether_ctl.if Removed vnic_service.c which was not in use anyways.

--- lib/ethersrv/ethersrv_debug.h
+++ lib/ethersrv/ethersrv_debug.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007, 2008, 2009, ETH Zurich.
+ * Copyright (c) 2007-11 ETH Zurich.
  * All rights reserved.
  *
  * This file is distributed under the terms in the attached LICENSE file.
@@ -23,6 +23,6 @@ void ethersrv_debug_printf(const char *fmt, ...);
 #define ETHERSRV_DEBUG(x...) printf("ETHERSRV: " x)
 #else
 #define ETHERSRV_DEBUG(x...) ((void)0)
-#endif                          // defined(ETHERSRV_SERVICE_DEBUG) || defined(GLOBAL_DEBUG)
+#endif // defined(ETHERSRV_SERVICE_DEBUG) || defined(GLOBAL_DEBUG)
 
-#endif                          // ETHERSRV_DEBUG_H_
+#endif // ETHERSRV_DEBUG_H_
