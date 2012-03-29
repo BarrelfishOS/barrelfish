3792953997e1da851ce3521f372a5f678097724c
Gerd Zellweger <zgerd@student.ethz.ch>
2012-03-29 17:19:40 +0200

    Don't use Kaluga on ARM and SCC for now. Need to think of better integration with these two architectures.

--- usr/spawnd/service.c
+++ usr/spawnd/service.c
@@ -571,7 +571,8 @@ static void export_cb(void *st, errval_t err, iref_t iref)
     if (err_is_fail(err)) {
         USER_PANIC_ERR(err, "nameservice_register failed");
     }
-#ifndef USE_KALUGA_DVM
+
+#if defined(USE_KALUGA_DVM) && (!defined(__arm__) || !defined(__scc__))
     // let the master know we are ready
     err = nsb_register_n(my_core_id, SERVICE_BASENAME);
     if (err_is_fail(err)) {
