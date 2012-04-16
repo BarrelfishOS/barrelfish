0b0846339a0e331e0341c1ec499ae19b6214e193
Antoine Kaufmann <antoinek@student.ethz.ch>
2012-04-16 23:10:24 +0200

    net_latency benchmark extended to run more benchmarks ---  usr/bench/net_latency/main.c |   51 ++++++++++++++++++++++++++++++++----------  1 files changed, 39 insertions(+), 12 deletions(-)

--- usr/bench/net_latency/main.c
+++ usr/bench/net_latency/main.c
@@ -17,34 +17,61 @@
 #include "sleep.h"
 
 
-static void start_run(uint8_t core)
+static void start_run(uint8_t core, int payload, int nocache, int read_incoming,
+                      int head_idx_wb)
 {
     errval_t r;
     domainid_t new_domain;
     //uint8_t code;
-    char* const argv[] = { "e10k_queue_elb", "queue=0", NULL };
+    char plsz[strlen("payload_size=0000") + 1];
+    char noca[strlen("elb_nocache=0") + 1];
+    char rdic[strlen("read_incoming=0") + 1];
+    char hiwb[strlen("head_idx_wb=0") + 1];
+    char prefix[128];
+
+    char* const argv[] = { "e10k_queue_elb", "queue=0", "runs=100",
+                           "dump_each=1", plsz, noca, rdic, hiwb, prefix, NULL};
+
+    sprintf(plsz, "payload_size=%d", payload);
+    sprintf(plsz, "elb_nocache=%d", nocache);
+    sprintf(rdic, "read_incoming=%d", read_incoming);
+    sprintf(hiwb, "head_idx_wb=%d", head_idx_wb);
+    sprintf(prefix, "elp_outprefix=%d,%d,%d,%d,%d,", core, payload, nocache,
+            read_incoming, head_idx_wb);
 
     r = spawn_program(core, argv[0], argv, NULL, SPAWN_NEW_DOMAIN, &new_domain);
     assert(err_is_ok(r));
 
     /*r = spawn_wait(new_domain, &code, false);
     assert(err_is_ok(r));*/
-    milli_sleep(5*1000);
+    milli_sleep(3*1000);
+    //spawn_wait(new_domain, &code, true);
 }
 
 
 int main(int argc, char **argv)
 {
-    uint8_t core = 0;
-    printf("Net latency benchmark start\n");
+    uint8_t core;
+    int payloadsz = 64;
+    int nocache = 0;
+    int read_incoming = 0;
+    int head_idx_wb = 0;
+    sleep_init();
 
-    /*if (!strncmp(argv[1], "startcore=", strlen("startcore="))) {
-        core = atol(argv[1] + strlen("startcore="));
-    }*/
-    for (; core < 16; core++) {
-        printf("Core %d:\n  ", core);
-        fflush(stdout);
-        start_run(core);
+    printf("Net latency benchmark start\n");
+    printf("%%  \"core\",\"payload\",\"nocache\",\"touch\",\"hiwb\","
+           "\"rtt\",\"time\"\n");
+    for (core = 0; core < 16; core++) {
+        for (payloadsz = 64; payloadsz < 1500; payloadsz *= 4) {
+            for (nocache = 0; nocache <= 1; nocache++) {
+                for (read_incoming = 0; read_incoming <= 1; read_incoming++) {
+                    for (head_idx_wb = 0; head_idx_wb <= 1; head_idx_wb++) {
+                        start_run(core, payloadsz, nocache, read_incoming,
+                                  head_idx_wb);
+                    }
+                }
+            }
+        }
     }
     printf("Net latency benchmark done\n");
     return 0;
