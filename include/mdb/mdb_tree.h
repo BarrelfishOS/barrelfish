36f6824f01e38d40894951731698cc0f76b0bda7
Mark Nevill <nevillm@ethz.ch>
2012-05-31 15:02:22 +0200

    Added copyright/license to libmdb headers.

--- include/mdb/mdb_tree.h
+++ include/mdb/mdb_tree.h
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2012, ETH Zurich.
+ * All rights reserved.
+ *
+ * This file is distributed under the terms in the attached LICENSE file.
+ * If you do not find this file, copies can be found by writing to:
+ * ETH Zurich D-INFK, Haldeneggsteig 4, CH-8092 Zurich. Attn: Systems Group.
+ */
+
 #ifndef LIBMDB_MDB_TREE_H
 #define LIBMDB_MDB_TREE_H
 
