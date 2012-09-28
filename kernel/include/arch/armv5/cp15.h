ed9d5181d09c73b3decdfaef9b8bc8045a839c3c
Kornilios Kourtis <kkourt@inf.ethz.ch>
2012-09-28 11:40:36 +0200

    armv5: move cp15_invalidate_i_and_d_caches() to a .c file
    This makes armv5 compile again.

--- kernel/include/arch/armv5/cp15.h
+++ kernel/include/arch/armv5/cp15.h
@@ -10,6 +10,10 @@
 #ifndef __CP15_H__
 #define __CP15_H__
 
+#include <kernel.h>
+
+void cp15_invalidate_i_and_d_caches(void);
+
 /**
  * \brief Read domain access control register
  */
@@ -62,35 +66,6 @@ static inline void cp15_write_ttbr(lpaddr_t ttbr)
     __asm volatile(" mcr  p15, 0, %[ttbr], c2, c0, 0" :: [ttbr] "r" (ttbr));
 }
 
-/*
- * IXP2800_Hardware_Reference_Manual, p.94 i-cache
- * IXP2800_Hardware_Reference_Manual, p.105 D-cache
- */
-static inline void cp15_invalidate_i_and_d_caches(void)
-{
-    uint32_t tmp_mem;
-    uint32_t *tmp = &tmp_mem; //Use variable on stack as storage space. We need a safe place in virtual memory.
-
-    __asm volatile(
-                   //Clean (write back) D-cache
-                   "MOV r0, #1024 \n\t"
-                   "LOOP1: \n\t"
-                   "MCR p15, 0, r1, c7, c2, 5\n\t"
-                   "ADD r1, r1, #32\n\t"
-                   "SUBS r0, r0, #1\n\t"
-                   "BNE LOOP1\n\t"
-                   "MOV r0, #64\n\t"
-
-                   //Clean (write back) mini D-cache
-                   "LOOP2:\n\t"
-                   "MOV r2, %[tmp]\n\t" 
-                   "LDR r3, [r2], #32\n\t"
-                   "SUBS r0, r0, #1\n\t"
-                   "BNE LOOP2\n\t"
-                   "MCR p15, 0, r0, c7, c6, 0\n\t" //Invalidate D-cache
-                   "mcr  p15, 0, r1, c7, c5, 0 \n\t" //Invalidate i-cache
-                   ::[tmp] "r" (tmp) : "r0", "r1", "r2", "r3");
-}
 
 static inline uint32_t cp15_read_cache_status(void){
     uint32_t cache;
