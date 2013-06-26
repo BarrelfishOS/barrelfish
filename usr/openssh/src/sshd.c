c69292abd79147874f77e371378e2026d64d21a2
Raphael Fuchs <fuchs.raphael@gmail.com>
2013-06-26 20:26:32 +0200

    openssh: In Barrelfish domains started from menu.lst only have the program name in argv[0] (argv[0]). Checking for an absolute path would fail.

--- usr/openssh/src/sshd.c
+++ usr/openssh/src/sshd.c
@@ -1524,8 +1524,15 @@ main(int ac, char **av)
 
 	if (rexeced_flag || inetd_flag)
 		rexec_flag = 0;
+
+    /*
+     * In Barrelfish domains started from menu.lst only have the program name
+     * in av[0] (argv[0]). Checking for an absolute path would fail.
+     */
+#if !defined(BARRELFISH)
 	if (!test_flag && (rexec_flag && (av[0] == NULL || *av[0] != '/')))
 		fatal("sshd re-exec requires execution with an absolute path");
+#endif
 	if (rexeced_flag)
 		closefrom(REEXEC_MIN_FREE_FD);
 	else
