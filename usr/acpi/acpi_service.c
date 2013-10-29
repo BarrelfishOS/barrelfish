120692def596f4bf19037f1020d161dd664be111
Gerd Zellweger <mail@gerdzellweger.com>
2013-10-29 08:55:54 +0100

    Fix debug output in ACPI alloc range.

--- usr/acpi/acpi_service.c
+++ usr/acpi/acpi_service.c
@@ -26,13 +26,13 @@
 static void mm_alloc_range_proxy_handler(struct acpi_binding* b, uint8_t sizebits,
 		                                 genpaddr_t minbase, genpaddr_t maxlimit)
 {
-    ACPI_DEBUG("mm_alloc_range_proxy_handler: sizebits: %d, minbase: %lu maxlimit: %lu\n",
+    ACPI_DEBUG("mm_alloc_range_proxy_handler: sizebits: %d, minbase: 0x%"PRIxGENPADDR" maxlimit: 0x%"PRIxGENPADDR"\n",
                sizebits, minbase, maxlimit);
 
     struct capref devframe = NULL_CAP;
     errval_t err = mm_alloc_range(&pci_mm_physaddr, sizebits, minbase, maxlimit, &devframe, NULL);
     if (err_is_fail(err)) {
-    	DEBUG_ERR(err, "mm realloc range failed...\n");
+    	DEBUG_ERR(err, "mm alloc range failed...\n");
     }
 
     err = b->tx_vtbl.mm_alloc_range_proxy_response(b, NOP_CONT, devframe, err);
