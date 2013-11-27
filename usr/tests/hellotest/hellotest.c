4837d81214de3427b79b7a54620477421606ba09
Gerd Zellweger <mail@gerdzellweger.com>
2013-11-27 18:50:20 +0100

    Changed hellotest to continue to run forever.
    Helps when testing shutdown of cores.

--- usr/tests/hellotest/hellotest.c
+++ usr/tests/hellotest/hellotest.c
@@ -3,10 +3,16 @@
 
 int main(int argc, char *argv[])
 {
-  debug_printf("Hello world (debug_printf)\n");
-  printf("Hello world (normal printf)\n");
-  for (int i = 0;i < argc; i ++) {
-    printf("arg[%d] = %s\n", i, argv[i]);
-  }
+    volatile int a = 0;
+    while(1) {
+      if ((a++ % 10000000000) == 0) {
+          debug_printf("Hello world (debug_printf)\n");
+          printf("Hello world (normal printf)\n");
+          for (int i = 0;i < argc; i ++) {
+            printf("arg[%d] = %s\n", i, argv[i]);
+          }
+      }
+    }
+
   return EXIT_SUCCESS;
 }
