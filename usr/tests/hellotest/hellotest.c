fe767dce663a7ebf0386c5b658032aee3a69d5b7
Simon Gerber <simon.gerber@inf.ethz.ch>
2014-04-25 19:21:51 +0200

    Reverted hellotest to upstream version.

--- usr/tests/hellotest/hellotest.c
+++ usr/tests/hellotest/hellotest.c
@@ -1,21 +1,12 @@
 #include <stdio.h>
 #include <barrelfish/barrelfish.h>
-#include <bench/bench.h>
-#include <if/monitor_blocking_rpcclient_defs.h>
-
-#define ITERATIONS 100000000
 
 int main(int argc, char *argv[])
 {
-    bench_init();
-    while(1) {
-
-        uint64_t start = bench_tsc();
-        for (volatile int i = 0; i < ITERATIONS; i++);
-        uint64_t end = bench_tsc();
-
-        printf("%lu %lu %lu\n", ITERATIONS/(end-start), end-start, bench_tsc_to_ms(end-start));
-    }
-
+  debug_printf("Hello world (debug_printf)\n");
+  printf("Hello world (normal printf)\n");
+  for (int i = 0;i < argc; i ++) {
+    printf("arg[%d] = %s\n", i, argv[i]);
+  }
   return EXIT_SUCCESS;
 }
