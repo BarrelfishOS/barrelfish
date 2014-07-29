a7187ead62f154663782187f2c3e38266a74483b
Reto Achermann <acreto@student.ethz.ch>
2014-07-29 18:08:27 +0200

    Adapted the spinlock implementation to the xeon phi.

--- include/arch/x86/barrelfish_kpi/spinlocks_arch.h
+++ include/arch/x86/barrelfish_kpi/spinlocks_arch.h
@@ -22,21 +22,38 @@ typedef volatile uint32_t spinlock_t;
 
 static inline void acquire_spinlock(spinlock_t *lock)
 {
+#ifdef __k1om__
+    /* The Xeon Phi does not support pause instruction. we use delay instead
+     * which does the same thing as pause but with a variable amount of delay.
+     * 750 cycles for now.
+     */
+    uint32_t wait = 750;
     __asm__ __volatile__("0:\n\t"
-                         "cmpl $0, %0\n\t"
-                         "je 1f\n\t"
-                         "pause\n\t"
-                         "jmp 0b\n\t"
-                         "1:\n\t"
-                         "lock btsl $0, %0\n\t"
-                         "jc 0b\n\t"
-                         : "+m" (*lock) : : "memory", "cc");
+                    "cmpl $0, %0\n\t"
+                    "je 1f\n\t"
+                    "delay %1\n\t"
+                    "jmp 0b\n\t"
+                    "1:\n\t"
+                    "lock btsl $0, %0\n\t"
+                    "jc 0b\n\t"
+                    : "+m" (*lock), "=r"(wait) : : "memory", "cc");
+#else
+    __asm__ __volatile__("0:\n\t"
+                    "cmpl $0, %0\n\t"
+                    "je 1f\n\t"
+                    "pause\n\t"
+                    "jmp 0b\n\t"
+                    "1:\n\t"
+                    "lock btsl $0, %0\n\t"
+                    "jc 0b\n\t"
+                    : "+m" (*lock) : : "memory", "cc");
+#endif
 }
 
 static inline void release_spinlock(spinlock_t *lock)
 {
-       __asm__ __volatile__("movl $0, %0\n\t"
-                         : "+m" (*lock) : : "memory" );
+    __asm__ __volatile__("movl $0, %0\n\t"
+                    : "+m" (*lock) : : "memory" );
 }
 
 #endif
