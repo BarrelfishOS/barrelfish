f267f304836b562d0f5bfc35e1186dcdb9bb576c
Simon Gerber <simon.gerber@hp.com>
2014-08-04 15:58:38 -0700

    Added test case to inspect processed argc/argv after libbarrelfish initialization.
    Signed-off-by: Simon Gerber <simon.gerber@hp.com>

---
+++ usr/tests/argtest/argtest.c
@@ -0,0 +1,23 @@
+/**
+ * \file
+ * \brief arguments test.
+ */
+
+/*
+ * Copyright (c) 2014, HP Labs.
+ * All rights reserved.
+ *
+ * This file is distributed under the terms in the attached LICENSE file.
+ * If you do not find this file, copies can be found by writing to:
+ * ETH Zurich D-INFK, Universitaetstr. 6, CH-8092 Zurich. Attn: Systems Group.
+ */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    for (int i = 0; i < argc; i++) {
+        printf("argv[%d] = %s\n", i, argv[i]);
+    }
+    return 0;
+}
