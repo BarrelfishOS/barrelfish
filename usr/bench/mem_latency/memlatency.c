910b2c5afc77d423f4b57d9da71d688a029a7aeb
Reto Achermann <acreto@student.ethz.ch>
2014-09-05 15:11:48 +0200

    mem_latency benchmark: rewritten the measuring loops.

--- usr/bench/mem_latency/memlatency.c
+++ usr/bench/mem_latency/memlatency.c
@@ -1,55 +1,96 @@
 /*
- * Copyright (c) 2009, 2010, 2011, ETH Zurich.
+ * Copyright (c) 2014 ETH Zurich.
  * All rights reserved.
  *
  * This file is distributed under the terms in the attached LICENSE file.
  * If you do not find this file, copies can be found by writing to:
- * ETH Zurich D-INFK, Haldeneggsteig 4, CH-8092 Zurich. Attn: Systems Group.
+ * ETH Zurich D-INFK, Universitaetsstrasse 6, CH-8092 Zurich. Attn: Systems Group.
  */
+
 #include <string.h>
 #include <stdlib.h>
 #include <limits.h>
 #include <barrelfish/barrelfish.h>
 #include <bench/bench.h>
-#include <barrelfish/sys_debug.h>
-
-#define EXPECT_SUCCESS(_err, msg...) \
-    if (err_is_fail(_err)) {USER_PANIC_ERR(_err, msg);}
-
-#define RUN_COUNT 2000
 
 /*
- * Cache Size
+ * ===========================================================================
+ * XXX: README
+ *
+ * The compilation of this file may take quite long due to heavily use of
+ * expansive makros in the loop (see next)
+ * ===========================================================================
  */
+
+
+/*
+ * ----------------------------------------------------------------------------
+ * Benchmark Settings
+ * ----------------------------------------------------------------------------
+ */
+
+/// the number of benchmark rounds to execute
+#define RUN_COUNT 1000
+
+/// number of loop iterations of 10k operations
+#define LOOP_ITERATIONS 100
+
+/// working set multiplier (workset = sizeof(LL_CACHE) * multiplier
+#define WORKSET_SIZE_MULT 8
+
+/*
+ * ----------------------------------------------------------------------------
+ * Machine Settings
+ * ----------------------------------------------------------------------------
+ */
+
 #ifdef __k1om__
 #define CHACHE_L1_SIZE (32UL * 1024)
 #define CHACHE_L2_SIZE (512UL * 1024)
 #define CHACHE_L3_SIZE (28UL*1024*1024 + 512UL * 1024)
 #define CHACHE_LL_SIZE (CHACHE_L3_SIZE)
 #define CHACHE_LINE_SIZE 64
-#define WORKSET_SIZE_MULT 8
 #define WORKSET_SIZE (WORKSET_SIZE_MULT * CHACHE_LL_SIZE)
 #define NUMA_NODE_MAX 0
 #define NUME_MEM_PER_NODE (6UL * 1024 * 1024 * 1024)
 #define DIMENSIONS 2
 #else
-#define CHACHE_L1_SIZE (320UL * 1024)
-#define CHACHE_L2_SIZE (2UL*1024*1024 + 512UL * 1024)
+#define CHACHE_L1_SIZE (32UL * 1024)
+#define CHACHE_L2_SIZE (256UL * 1024)
 #define CHACHE_L3_SIZE (25UL*1024*1024)
 #define CHACHE_LL_SIZE (CHACHE_L3_SIZE)
 #define CHACHE_LINE_SIZE 64
-#define WORKSET_SIZE_MULT 16
 #define WORKSET_SIZE (WORKSET_SIZE_MULT * CHACHE_LL_SIZE)
 #define NUMA_NODE_MAX 1
 #define NUME_MEM_PER_NODE (128UL * 1024 * 1024 * 1024)
 #define DIMENSIONS 3
 #endif
 
+
+#define EXPECT_SUCCESS(_err, msg...) \
+    if (err_is_fail(_err)) {USER_PANIC_ERR(_err, msg);}
+
+
+#define NEXT(_e) (_e) = (_e)->next;
+#define NEXT_5(_e) NEXT(_e) NEXT(_e) NEXT(_e) NEXT(_e) NEXT(_e)
+#define NEXT_10(_e) NEXT_5(_e) NEXT_5(_e)
+#define NEXT_50(_e) NEXT_10(_e) NEXT_10(_e) NEXT_10(_e) NEXT_10(_e) NEXT_10(_e)
+#define NEXT_100(_e) NEXT_50(_e) NEXT_50(_e)
+#define NEXT_500(_e) NEXT_100(_e) NEXT_100(_e) NEXT_100(_e) NEXT_100(_e) NEXT_100(_e)
+#define NEXT_1000(_e) NEXT_500(_e) NEXT_500(_e)
+
+
+
 struct elem {
     struct elem *next;
     uint32_t val;
     uint8_t pad[CHACHE_LINE_SIZE - sizeof(void *) - sizeof(uint32_t)];
 };
+
+struct celem {
+    struct celem *next;
+};
+
 
 uint32_t *elem_id = NULL;
 
@@ -75,7 +116,11 @@ static inline cycles_t calculate_time(cycles_t tsc_start,
 
 static uint8_t numa_node(void)
 {
+#ifdef __k1om__
+    return 0;
+#else
     return (disp_get_core_id() > 19);
+#endif
 }
 
 static void generate_shuffle(size_t num)
@@ -116,10 +161,11 @@ static void init_memory(struct elem *mem,
 
     /* do the linkage */
     struct elem *head = &mem[elem_id[0]];
-    for (uint32_t i = 1; i < num + 1; ++i) {
+    for (uint32_t i = 1; i < num; ++i) {
         head->next = &mem[elem_id[i]];
         head = head->next;
     }
+    mem[elem_id[num-1]].next = &mem[elem_id[0]];
 }
 
 static void alloc_memory(struct elem **mem,
@@ -155,6 +201,36 @@ static void alloc_memory(struct elem **mem,
     }
 }
 
+static cycles_t run_benchmark(void *buffer, volatile void **ret_elem)
+{
+
+
+
+    volatile struct elem *e = buffer;
+
+    cycles_t tsc_start = bench_tsc();
+
+    for (uint32_t i = 0; i < LOOP_ITERATIONS; ++i) {
+        NEXT_1000(e);
+        NEXT_1000(e);
+        NEXT_1000(e);
+        NEXT_1000(e);
+        NEXT_1000(e);
+        NEXT_1000(e);
+        NEXT_1000(e);
+        NEXT_1000(e);
+        NEXT_1000(e);
+        NEXT_1000(e);
+    }
+    cycles_t tsc_end = bench_tsc();
+
+    if (ret_elem) {
+        *ret_elem = e;
+    }
+
+    return calculate_time(tsc_start, tsc_end) / (LOOP_ITERATIONS * 10000);
+}
+
 int main(int argc,
          char *argv[])
 {
@@ -162,7 +238,7 @@ int main(int argc,
     bench_init();
 
     debug_printf("memlatency benchmark started...Workset size: %lu MBytes\n",
-    WORKSET_SIZE >> 20);
+                 WORKSET_SIZE >> 20);
 
     if (numa_node()) {
         USER_PANIC("must be run on numa node 0\n");
@@ -190,10 +266,14 @@ int main(int argc,
     /*
      * do cache allocations
      */
-    size_t cache_elements = CHACHE_L1_SIZE / sizeof(uint64_t);
+    size_t cache_elements = (CHACHE_L1_SIZE / sizeof(struct celem)) >> 2;
     debug_printf("initialize cached elements, num:%lu\n", cache_elements);
 
-    uint64_t *l1_elements = malloc(cache_elements);
+    struct celem *l1_elements = malloc((cache_elements)* sizeof(struct celem));
+    for (uint32_t i = 0; i < cache_elements - 1; ++i) {
+        l1_elements[i].next = &l1_elements[i+1];
+    }
+    l1_elements[cache_elements-1].next = l1_elements;
 
     /*
      * BENCHMARK
@@ -201,65 +281,46 @@ int main(int argc,
 
     debug_printf("starting benchmark %u rounds\n", RUN_COUNT);
 
-    size_t cached_outer_loop = num_elements / cache_elements;
-    size_t cached_num = (cached_outer_loop * cache_elements);
     bench_ctl_t *ctl;
     cycles_t result[DIMENSIONS];
-    cycles_t tsc_start, tsc_end;
 
     uint32_t round = 0;
 
     ctl = bench_ctl_init(BENCH_MODE_FIXEDRUNS, DIMENSIONS, RUN_COUNT);
     do {
+        volatile void *element;
+        result[0] = run_benchmark(&ll_elements[elem_id[0]], &element);
+                /*  make sure the loop is not optimized away */
 
-        volatile struct elem *e = &ll_elements[elem_id[0]];
-        tsc_start = bench_tsc();
-        for (uint32_t i = 0; i < num_elements; ++i) {
-            e = e->next;
-        }
-        tsc_end = bench_tsc();
-
-        /*  make sure the loop is not optimized away */
-        if (e && e->val == 0) {
-            debug_printf("val was: %u\n", e->val);
+        /* just a access to the variable */
+        if (!element) {
+            debug_printf("element %p was null.\n", element);
         }
 
-        result[0] = calculate_time(tsc_start, tsc_end) / num_elements;
 
 #ifndef __k1om__
-        e = &ll_elements_numa[elem_id[0]];
-        tsc_start = bench_tsc();
-        for (uint32_t i = 0; i < num_elements; ++i) {
-            e = e->next;
-        }
-        tsc_end = bench_tsc();
+        result[2] = run_benchmark(&ll_elements_numa[elem_id[0]], &element);
 
-        /*  make sure the loop is not optimized away */
-        if (e && e->val == 0) {
-            debug_printf("val was: %u\n", e->val);
+        /* just a access to the variable */
+        if (!element) {
+            debug_printf("element %p was null.\n", element);
         }
-
-        result[2] = calculate_time(tsc_start, tsc_end) / num_elements;
 #endif
+
+        volatile struct celem *e2 = l1_elements;
+
         /* warmup cache */
         for (uint32_t i = 0; i < cache_elements; ++i) {
-            l1_elements[i] = i;
+            NEXT_1000(e2);
         }
 
-        volatile uint32_t val = 0;
-        tsc_start = bench_tsc();
-        for (uint32_t j = 0; j < cached_outer_loop; ++j) {
-            for (uint32_t i = 0; i < cache_elements; ++i) {
-                val = l1_elements[i];
-            }
-        }
-        tsc_end = bench_tsc();
+        e2 = l1_elements;
+        result[1] = run_benchmark(l1_elements, &element);
 
-        /* make sure the result is not optimized away */
-        if (val == 0) {
-            debug_printf("val was: %u\n", val);
+        /* just a access to the variable */
+        if (!element) {
+            debug_printf("element %p was null.\n", element);
         }
-        result[1] = calculate_time(tsc_start, tsc_end) / cached_num;
 
         debug_printf("round: %u of %u\n", ++round, RUN_COUNT);
     } while (!bench_ctl_add_run(ctl, result));
