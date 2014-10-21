6a8732795b96b28eb7ca96f0e673e8c64bf40286
Gerd Zellweger <mail@gerdzellweger.com>
2014-10-21 10:32:22 +0200

    Added periodic print test to test parking/stop/update scenarios.

---
+++ usr/tests/hellotest/printperiodic.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <barrelfish/barrelfish.h>
+#include <bench/bench.h>
+#include <if/monitor_blocking_rpcclient_defs.h>
+
+#define ITERATIONS 100000000
+
+int main(int argc, char *argv[])
+{
+    bench_init();
+
+    while(1) {
+
+        uint64_t start = bench_tsc();
+        for (volatile int i = 0; i < ITERATIONS; i++);
+        uint64_t end = bench_tsc();
+
+        printf("On core %"PRIuCOREID" %lu %lu %lu\n", disp_get_current_core_id(), 
+                ITERATIONS/(end-start), end-start, bench_tsc_to_ms(end-start));
+    }
+
+  return EXIT_SUCCESS;
+}
+
