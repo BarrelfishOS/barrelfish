9d1f584a51eea365cb85e0833a336e8becabfdae
Gerd Zellweger <mail@gerdzellweger.com>
2014-10-21 11:45:06 +0200

    Tests to update/park/stop core working.

--- tools/harness/tests/coreboottest.py
+++ tools/harness/tests/coreboottest.py
@@ -9,9 +9,11 @@
 # ETH Zurich D-INFK, Universitätstrasse 6, CH-8092 Zurich. Attn: Systems Group.
 ##########################################################################
 
-import tests, debug, time
+import tests, debug, time, pexpect
 from common import InteractiveTest
 from results import PassFailResult
+
+START_CPU_DRIVER = "Barrelfish CPU driver starting"
 
 @tests.add_test
 class StopCoreTest(InteractiveTest):
@@ -28,23 +30,27 @@ class StopCoreTest(InteractiveTest):
         self.wait_for_fish()
 
         time.sleep(5)
-        core = 2
-        debug.verbose("Stopping core %s." % core)
-        self.console.sendline("x86boot stop %s" % core)
+        self.core = 2
+        debug.verbose("Stopping core %s." % self.core)
+        self.console.sendline("x86boot stop %s" % self.core)
         
+        # Stop core
         debug.verbose("Wait until core is down.")
-        self.console.expect("Core %s stopped." % core)
-        
+        self.console.expect("Core %s stopped." % self.core)
         self.wait_for_prompt()
-        time.sleep(5)
+
+        # Make sure app is no longer running
+        i = self.console.expect(["On core %s" % self.core, pexpect.TIMEOUT], timeout=10)
+        if i == 0:
+            raise Exception("periodicprint still running, did we not shut-down the core?")
 
     def process_data(self, testdir, rawiter):
         passed = True
+        stopped = False
         for line in rawiter:
             if "user page fault in" in line:
                 passed = False
                 break
-
         return PassFailResult(passed)
 
 
@@ -54,20 +60,22 @@ class UpdateKernelTest(InteractiveTest):
 
     name = 'update_kernel'
 
+    def get_modules(self, build, machine):
+        modules = super(UpdateKernelTest, self).get_modules(build, machine)
+        modules.add_module("periodicprint", args=["core=2"])
+        return modules
+
     def interact(self):
+        self.core = 2
         self.wait_for_fish()
 
-        debug.verbose("Stopping core 1.")
-        self.console.sendline("x86boot stop 1")
-        debug.verbose("Wait until core is down.")
-        self.console.expect("Power it down...")
-
+        # Reboot core
+        self.console.sendline("x86boot update %s" % self.core)
+        self.console.expect(START_CPU_DRIVER)
         self.wait_for_prompt()
-        self.console.sendline("x86boot up 1")
-        self.console.expect("Core 1 up")
 
-        self.wait_for_prompt()
-        time.sleep(5)
+        # Make sure app is still running
+        self.console.expect("On core %s" % self.core)
 
     def process_data(self, testdir, rawiter):
         passed = True
@@ -75,24 +83,38 @@ class UpdateKernelTest(InteractiveTest):
             if "user page fault in" in line:
                 passed = False
                 break
-
         return PassFailResult(passed)
 
 
 @tests.add_test
 class ParkOSNodeTest(InteractiveTest):
     '''Park an OSNode on a core.'''
-
     name = 'park_osnode'
+
+    def get_modules(self, build, machine):
+        modules = super(ParkOSNodeTest, self).get_modules(build, machine)
+        modules.add_module("periodicprint", args=["core=2"])
+        return modules
 
     def interact(self):
         self.wait_for_fish()
+        
+        self.core = 2
+        self.target_core = 4
 
-        debug.verbose("Stopping core 1.")
-        self.console.sendline("x86boot stop 1")
+        self.console.expect("On core %s" % self.core)
+
+        # Stop
+        debug.verbose("Stopping core %s." % self.core)
+        self.console.sendline("x86boot stop %s" % self.core)
         self.wait_for_prompt()
-        self.console.sendline("x86boot give 1 2")
-        time.sleep(5)
+
+        # Park
+        debug.verbose("Transfer OSNode from %s to %s." % (self.core, self.target_core))
+        self.console.sendline("x86boot give %s %s" % (self.core, self.target_core))
+        self.wait_for_prompt()
+
+        self.console.expect("On core %s" % self.target_core)
 
     def process_data(self, testdir, rawiter):
         passed = True
@@ -100,6 +122,5 @@ class ParkOSNodeTest(InteractiveTest):
             if "user page fault in" in line:
                 passed = False
                 break
-
         return PassFailResult(passed)
 
