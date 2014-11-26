f2c10df43df68a76b169f747cd8d3bd725683221
Gerd Zellweger <mail@gerdzellweger.com>
2014-11-26 13:23:47 +0100

    Disable debugging.

--- usr/kaluga/debug.h
+++ usr/kaluga/debug.h
@@ -15,7 +15,7 @@
 #ifndef KALUGA_DEBUG_H_
 #define KALUGA_DEBUG_H_
 
-#define KALUGA_SERVICE_DEBUG 1
+//#define KALUGA_SERVICE_DEBUG 1
 
 #if defined(KALUGA_SERVICE_DEBUG) || defined(GLOBAL_DEBUG)
 #define KALUGA_DEBUG(x...) debug_printf(x)
