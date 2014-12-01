e75d4809d1e5208e0d0ee2ecea77cd832dc1b0bb
Reto Achermann <reto.achermann@inf.ethz.ch>
2014-12-01 10:34:08 +0100

    added a simple check for C++ exceptions

--- usr/tests/cxx/stl_exceptions.cpp
+++ usr/tests/cxx/stl_exceptions.cpp
@@ -10,6 +10,29 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <exception>
 
 #include "cxxtest.hpp"
 
+#include <iostream>
+
+using namespace std;
+
+class myexception: public exception
+{
+  virtual const char* what() const throw()
+  {
+    return "My exception happened";
+  }
+} myex;
+
+void stl_exception_test(void) {
+  try
+  {
+    throw myex;
+  }
+  catch (exception& e)
+  {
+    cout << e.what() << '\n';
+  }
+}
