b5fbca97df1db87acac83154262f8e1f3e1e45d1
Reto Achermann <acreto@student.ethz.ch>
2014-12-13 09:23:33 +0100

    symbolic target: bugfix for GM targets with multiple architectures

--- hake/symbolic_targets.mk
+++ hake/symbolic_targets.mk
@@ -637,7 +637,7 @@ define \n
 endef
 
     #%/generated/triangle_counting.{h, cc} : tools/bin/gm_comp
-$(ARCH)/usr/green-marl/%.cc : tools/bin/gm_comp $(GM_APPS)/%.gm
+$(HAKE_ARCHS)/usr/green-marl/%.cc : tools/bin/gm_comp $(GM_APPS)/%.gm
 	$(foreach a,$(HAKE_ARCHS), \
 		mkdir -p $(a)/usr/green-marl ${\n}\
 		mkdir -p $(a)/include/green-marl ${\n}\
