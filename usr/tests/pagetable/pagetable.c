bf058cd0de3ea516b0bcb47dd51fefd8f4a8f553
Gerd Zellweger <mail@gerdzellweger.com>
2015-03-03 16:45:02 +0100

    Allowing to clear dirty bits from user-space and rewrite page table test
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

--- usr/tests/pagetable/pagetable.c
+++ usr/tests/pagetable/pagetable.c
@@ -5,9 +5,32 @@
 #include <stdio.h>
 #include <inttypes.h>
 #include <barrelfish/barrelfish.h>
+#include <barrelfish/invocations_arch.h>
+
+
+static inline errval_t clean_dirty_bits(struct capref vnode) {
+
+    struct sysret sysret = cap_invoke1(vnode, VNodeCmd_CleanDirtyBits);
+    assert(ret != NULL);
+    return sysret.error;
+}
+
+static inline errval_t invoke_vnode_identify(struct capref vnode,
+                                             struct frame_identity *ret)
+{
+    struct sysret sysret = cap_invoke1(vnode, VNodeCmd_Identify);
+    assert(ret != NULL);
+    if (err_is_ok(sysret.error)) {
+        ret->base = sysret.value & (~BASE_PAGE_MASK);
+        ret->bits = sysret.value & BASE_PAGE_MASK;
+        return sysret.error;
+    }
+    ret->base = 0;
+    ret->bits = 0;
+    return sysret.error;
+}
 
 static void print_vnodes(struct vnode* current, int depth) {
-
     char pad[depth+1];
     for (int i=0; i<depth; i++) {
         pad[i] = ' ';
@@ -37,6 +60,64 @@ static void print_vnodes(struct vnode* current, int depth) {
     }
 }
 
+static void find_pagetables(struct vnode* current) {
+    char capbuffer[1024];
+
+    while(current != NULL) {
+        if (current->is_vnode) {
+            struct capability ret;
+            errval_t err = debug_cap_identify(current->u.vnode.cap, &ret);
+            if (err_is_fail(err)) {
+                USER_PANIC_ERR(err, "debug_cap_identify failed.");
+            }
+
+            if (ret.type == ObjType_VNode_x86_64_ptable) {
+                printf("%s:%s:%d: we have a pagetable\n", __FILE__, __FUNCTION__, __LINE__);
+
+                struct frame_identity id = { .base = 0, .bits = 0 };
+                err = invoke_frame_identify(current->u.vnode.cap, &id);
+                if (err_is_fail(err)) {
+                    USER_PANIC_ERR(err, "Invoke vnode identify failed.");
+                }
+
+                struct capref ptable_copy;
+                err = slot_alloc(&ptable_copy);
+                if (err_is_fail(err)) {
+                    USER_PANIC_ERR(err, "Failed to allocate slot");
+                }
+
+                err = cap_copy(ptable_copy, current->u.vnode.cap);
+                if (err_is_fail(err)) {
+                    DEBUG_ERR(err, "cap_copy failed");
+                }
+
+                genpaddr_t *ptable = NULL;
+                err = vspace_map_one_frame((void**)&ptable, (size_t)1<<id.bits,
+                        ptable_copy, NULL, NULL);
+                if (err_is_fail(err)) {
+                    USER_PANIC_ERR(err, "Can not map the frame.");
+                }
+
+                debug_print_cap_at_capref(capbuffer, 1024, current->u.vnode.cap);
+                printf("vnode: cap=%s\n", capbuffer);
+                for(int i=0; i<10; i++) {
+                    bool is_dirty = (ptable[i] & X86_64_PTABLE_DIRTY) > 0;
+                    printf("retaddr[%d] = 0x%"PRIxGENPADDR" is_dirty = %d\n", i, ptable[i], is_dirty);
+                    clean_dirty_bits(current->u.vnode.cap);
+                }
+
+                //printf("%s:%s:%d: \n", __FILE__, __FUNCTION__, __LINE__);
+            }
+
+            find_pagetables(current->u.vnode.children);
+        }
+        else {
+            // Ignore frames
+        }
+        current = current->next;
+    }
+}
+
 int main(int argc, char *argv[])
 {
     errval_t err;
@@ -52,8 +133,9 @@ int main(int argc, char *argv[])
            __FILE__, __FUNCTION__, __LINE__);
     struct vnode* current = &x86->root;
     print_vnodes(current, 0);
+    find_pagetables(current);
 
-    current = &x86->root;
+    /*current = &x86->root;
     char capbuffer[1024];
 
     struct capref pml4 = current->u.frame.cap;
@@ -78,12 +160,9 @@ int main(int argc, char *argv[])
     lvaddr_t* ptable = (lvaddr_t*)retaddr;
     printf("retaddr[0] = 0x%"PRIxGENPADDR"\n", ptable[0]);
     printf("retaddr[1] = 0x%"PRIxGENPADDR"\n", ptable[1]);
+    printf("retaddr[1] = 0x%"PRIxGENPADDR"\n", ptable[2]);
+    printf("retaddr[1] = 0x%"PRIxGENPADDR"\n", ptable[3]);
+    printf("retaddr[1] = 0x%"PRIxGENPADDR"\n", ptable[4]);*/
 
-//    return vspace_map_one_frame_attr(retaddr, size, frame,
-//                                     VREGION_FLAGS_READ_WRITE, retmemobj,
-//                                     retvregion);
-
-
-    //printf("%s:%s:%d: pmap = %p\n", __FILE__, __FUNCTION__, __LINE__, pmap);
     return EXIT_SUCCESS;
 }
