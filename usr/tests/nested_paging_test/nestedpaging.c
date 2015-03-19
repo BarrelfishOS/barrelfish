44af8b78677ea022619d13ef589e2d40e57ac343
Simon Gerber <simon.gerber@inf.ethz.ch>
2015-03-19 20:56:22 +0100

    T81: Fix copyright notice in nested_paging_test.
    Signed-off-by: Simon Gerber <simon.gerber@inf.ethz.ch>

--- usr/tests/nested_paging_test/nestedpaging.c
+++ usr/tests/nested_paging_test/nestedpaging.c
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2015, ETH Zurich.
+ * All rights reserved.
+ *
+ * This file is distributed under the terms in the attached LICENSE file.
+ * If you do not find this file, copies can be found by writing to:
+ * ETH Zurich D-INFK, Universitaetstr 6, CH-8092 Zurich. Attn: Systems Group.
+ */
+
 #include <stdio.h>
 #include <inttypes.h>
 #include <barrelfish/barrelfish.h>
