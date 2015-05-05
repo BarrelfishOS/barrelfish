d58dfb6172e6f1176a55540473fe029ee94f423a
Simon Gerber <simon.gerber@inf.ethz.ch>
2015-05-05 19:14:28 +0200

    harness: Add babybel4 to eth machines.
    Signed-off-by: Simon Gerber <simon.gerber@inf.ethz.ch>

--- tools/harness/machines/eth_machinedata.py
+++ tools/harness/machines/eth_machinedata.py
@@ -140,6 +140,15 @@ machines = dict({
                 'tickrate'        : 2500,
                 'boot_timeout'    : 360},
 
+    'babybel4': {'ncores'          : 20,
+                'machine_name'    : 'babybel4',
+                'bootarch'        : 'x86_64',
+                'buildarchs'      : ['x86_64', 'x86_32'],
+                'cores_per_socket': 10,
+                'perfcount_type'  : 'intel',
+                'tickrate'        : 2500,
+                'boot_timeout'    : 360},
+
     'nos4-32'   : {'ncores'      : 4,
                    'machine_name' : 'nos4',
                    'bootarch' : 'x86_32',
@@ -272,6 +281,14 @@ machines = dict({
                    'tickrate'        : 2500,
                    'boot_timeout'    : 360},
 
+    'babybel4-32': {'ncores'          : 20,
+                   'machine_name'    : 'babybel4',
+                   'bootarch'        : 'x86_32',
+                   'buildarchs'      : ['x86_64', 'x86_32'],
+                   'cores_per_socket': 10,
+                   'perfcount_type'  : 'intel',
+                   'tickrate'        : 2500,
+                   'boot_timeout'    : 360},
 
     # SK: For Python 2.7
     # }.items() + { 
