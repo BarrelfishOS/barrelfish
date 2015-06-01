357e240ac3736fbf8c84c1a09976eb0d8b9de131
Simon Gerber <simon.gerber@inf.ethz.ch>
2015-06-01 15:28:24 +0200

    remove old newlib.
    Signed-off-by: Simon Gerber <simon.gerber@inf.ethz.ch>

--- lib/newlib-1.19.0/newlib/libc/include/unistd.h
+++
@@ -1,8 +0,0 @@
-#error This header is not compatible with Barrelfish! Use the Barrelfish version in include/unistd.h instead.
-
-#ifndef _UNISTD_H_
-#define _UNISTD_H_
-
-# include <sys/unistd.h>
-
-#endif /* _UNISTD_H_ */
