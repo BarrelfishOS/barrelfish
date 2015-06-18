9bf9d161a0c006edaafa3586e464cd2a4bc2a67b
Reto Achermann <reto.achermann@inf.ethz.ch>
2015-06-18 17:55:44 +0200

    testconcurrent: reordering includes
    Signed-off-by: Reto Achermann <reto.achermann@inf.ethz.ch>

--- usr/tests/testconcurrent/testconcurrent.c
+++ usr/tests/testconcurrent/testconcurrent.c
@@ -10,9 +10,10 @@
  * If you do not find this file, copies can be found by writing to:
  * ETH Zurich D-INFK, Haldeneggsteig 4, CH-8092 Zurich. Attn: Systems Group.
  */
+
+#include <stdio.h>
 #include <concurrent/arch/cas.h>
 #include <concurrent/linked_list.h>
-#include <stdio.h>
 
 static struct ll_element *list_head;
 static struct ll_element *list_tail;
