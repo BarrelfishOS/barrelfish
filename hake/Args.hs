b71ab6d0bc1c020546e5daa6b9c15de4b8bec988
Moritz Hoffmann <moritz.hoffmann@inf.ethz.ch>
2015-11-02 18:36:14 +0100

    hake: Remove unicode characters (ä -> ae) in header.

--- hake/Args.hs
+++ hake/Args.hs
@@ -4,7 +4,7 @@
 --
 -- This file is distributed under the terms in the attached LICENSE file.
 -- If you do not find this file, copies can be found by writing to:
--- ETH Zurich D-INFK, Universitätstasse 6, CH-8092 Zurich. Attn: Systems Group.
+-- ETH Zurich D-INFK, Universitaetstasse 6, CH-8092 Zurich. Attn: Systems Group.
 --
 -- Arguments to major Hake targets
 -- 
