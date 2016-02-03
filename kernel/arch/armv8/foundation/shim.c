38b1f3e0068a84a3a3a0ead2d9df8374baf7a317
David Cock <david.cock@inf.ethz.ch>
2016-02-03 14:57:37 +0100

    Semihosted syscalls for the ARM FM port
    Signed-off-by: David Cock <david.cock@inf.ethz.ch>

--- kernel/arch/armv8/foundation/shim.c
+++ kernel/arch/armv8/foundation/shim.c
@@ -3,6 +3,8 @@
 #include <offsets.h>
 #include <serial.h>
 
+#include <foundation/semihost.h>
+
 /*
  * Kernel stack.
  *
@@ -12,13 +14,39 @@
 uintptr_t kernel_stack[KERNEL_STACK_SIZE/sizeof(uintptr_t)]
     __attribute__ ((aligned(16)));
 
-void shim_entry(void) __attribute__((noreturn));
+void shim_entry(void) __attribute__((noreturn,section(".text.shim")));
+
+struct semihost_cmdline {
+    char *buf;
+    uint64_t length;
+};
+
+int semihost_get_cmdline(struct semihost_cmdline *cmdline)
+    __attribute__((noinline));
+
+int
+semihost_get_cmdline(struct semihost_cmdline *cmdline) {
+    return semihost_syscall(0x15, cmdline);
+}
+
+const char test_string[] = "test\n";
 
 void
 shim_entry(void) {
+    struct semihost_cmdline cmdline;
+    char cmdline_buf[256];
+
     serial_early_init(serial_console_port);
 
     printf("Shim loader starting.\n");
+
+    semihost_syscall(0x04, (void *)test_string);
+
+    cmdline.buf= cmdline_buf;
+    cmdline.length= 256;
+    int r= semihost_get_cmdline(&cmdline);
+    printf("%d %p %p %d\n", r, cmdline_buf, cmdline.buf, cmdline.length);
+    printf("\"%s\"\n", cmdline.buf);
 
     while(1);
 }
--- kernel/include/arch/armv8/foundation/semihost.h
+++ kernel/include/arch/armv8/foundation/semihost.h
@@ -3,6 +3,19 @@
 
 #include <stdint.h>
 
-uint64_t semihost_syscall(uint64_t op, void *param);
+static inline uint64_t semihost_syscall(uint64_t op, void *param) {
+    uint64_t ret;
+
+    __asm volatile(
+        "mov x0, %[op];"
+        "mov x1, %[param];"
+        "hlt 0xf000;"
+        "mov %[ret], x0;"
+        : [ret] "=r" (ret)
+        : [op] "r" (op), [param] "r" (param)
+        : "x0", "x1");
+
+    return ret;
+}
 
 #endif /* __SEMIHOST_H */
