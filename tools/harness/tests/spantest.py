2ce398e64cab020c12d9505be94598465d276db7
Reto Achermann <reto.achermann@inf.ethz.ch>
2016-02-03 15:57:16 +0100

    harness: adding tests for span interleave and span exit
    Signed-off-by: Reto Achermann <reto.achermann@inf.ethz.ch>

--- tools/harness/tests/spantest.py
+++ tools/harness/tests/spantest.py
@@ -41,3 +41,59 @@ class SpanTest(TestCommon):
             if re.match(MATCH, line):
                 result = True
         return PassFailResult(result)
+
+@tests.add_test
+class SpanTestInterleaved(TestCommon):
+    '''Interleave span and thread create'''
+    name = "spantest_interleaved"
+
+    def setup(self, build, machine, testdir):
+        super(SpanTest, self).setup(build, machine, testdir)
+
+        # XXX: track number of cores booted and seen for is_finished()
+        self._ncores = machine.get_ncores()
+        self._nseen = 0
+
+    def get_modules(self, build, machine):
+        modules = super(SpanTest, self).get_modules(build, machine)
+        # span on all cores other than 0 -- matches spantest code
+        modules.add_module("tests/span-interleaved", [ machine.get_ncores() ])
+        return modules
+
+    def is_finished(self, line):
+        return re.match('span-interleaved.*SUCCESS.*', line)
+
+    def process_data(self, testdir, rawiter):
+        result = False
+        for line in rawiter:
+            if re.match('span-interleaved.*SUCCESS.*', line) :
+                result = True
+        return PassFailResult(result)
+
+@tests.add_test
+class SpanTestExit(TestCommon):
+    '''Span a program then exit and see if other dispatchers cleanup'''
+    name = "spantest_exit"
+
+    def setup(self, build, machine, testdir):
+        super(SpanTest, self).setup(build, machine, testdir)
+
+        # XXX: track number of cores booted and seen for is_finished()
+        self._ncores = machine.get_ncores()
+        self._nseen = 0
+
+    def get_modules(self, build, machine):
+        modules = super(SpanTest, self).get_modules(build, machine)
+        # span on all cores other than 0 -- matches spantest code
+        modules.add_module("tests/span-exit", [ machine.get_ncores() ])
+        return modules
+
+    def is_finished(self, line):
+        return re.match('span-exit.*DONE.*', line)
+
+    def process_data(self, testdir, rawiter):
+        result = True
+        for line in rawiter:
+            if re.match("kernel *: user page fault*span-exit*", line):
+                result = False
+        return PassFailResult(result)
