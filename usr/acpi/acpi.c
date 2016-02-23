d2960330132891002507cfed2e2d6dc60e0b4fb6
Moritz Hoffmann <moritz.hoffmann@inf.ethz.ch>
2016-02-23 13:40:32 +0100

    acpi: Update some copyrights.
    Signed-off-by: Moritz Hoffmann <moritz.hoffmann@inf.ethz.ch>

--- usr/acpi/acpi.c
+++ usr/acpi/acpi.c
@@ -4,12 +4,12 @@
  */
 
 /*
- * Copyright (c) 2007, 2008, 2009, 2010, ETH Zurich.
+ * Copyright (c) 2007, 2008, 2009, 2010, 2016 ETH Zurich.
  * All rights reserved.
  *
  * This file is distributed under the terms in the attached LICENSE file.
  * If you do not find this file, copies can be found by writing to:
- * ETH Zurich D-INFK, Haldeneggsteig 4, CH-8092 Zurich. Attn: Systems Group.
+ * ETH Zurich D-INFK, Universitaetsstrasse 6, CH-8092 Zurich. Attn: Systems Group.
  */
 
 #include <stdio.h>
