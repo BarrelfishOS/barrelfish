d516e109cdd8067ffc074dfe6482a384e7b85583
Moritz Hoffmann <moritz.hoffmann@inf.ethz.ch>
2016-02-23 13:41:34 +0100

    skb: Update copyright notices.
    Signed-off-by: Moritz Hoffmann <moritz.hoffmann@inf.ethz.ch>

--- usr/skb/skb_main.c
+++ usr/skb/skb_main.c
@@ -1,10 +1,10 @@
 /*
- * Copyright (c) 2007, 2008, 2009, 2011, ETH Zurich.
+ * Copyright (c) 2007, 2008, 2009, 2011, 2016, ETH Zurich.
  * All rights reserved.
  *
  * This file is distributed under the terms in the attached LICENSE file.
  * If you do not find this file, copies can be found by writing to:
- * ETH Zurich D-INFK, Haldeneggsteig 4, CH-8092 Zurich. Attn: Systems Group.
+ * ETH Zurich D-INFK, Universitaetsstrasse 6, CH-8092 Zurich. Attn: Systems Group.
  */
 
 
