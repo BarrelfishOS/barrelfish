99541da335c42ae05af9b327b368e08f37dd863f
Simon Gerber <simon.gerber@inf.ethz.ch>
2016-04-20 13:51:14 +0200

    T191: write multi retype test and add test_retype2 in platforms/Hakefile
    Signed-off-by: Simon Gerber <simon.gerber@inf.ethz.ch>

--- tools/harness/tests/caps_next.py
+++ tools/harness/tests/caps_next.py
@@ -1,5 +1,6 @@
 
 import tests
+import re
 from common import TestCommon
 from results import PassFailResult
 
@@ -25,3 +26,38 @@ class Retype2Test(TestCommon):
                 results = results.strip()
                 passed = results == "0"
         return PassFailResult(passed)
+
+@tests.add_test
+class Retype2MultiTest(TestCommon):
+    '''test new retype code'''
+    name = "retype2_multi"
+
+    def setup(self,build,machine,testdir):
+        super(Retype2MultiTest, self).setup(build,machine,testdir)
+        self._ncores = machine.get_ncores()
+        self._nseen = 0
+
+    def get_modules(self, build, machine):
+        modules = super(Retype2MultiTest, self).get_modules(build, machine)
+        for core in machine.get_coreids():
+            modules.add_module("test_retype2", ["core=%d" % core])
+        return modules
+
+    def is_finished(self, line):
+        if line.startswith("retype2: result:"):
+            self._nseen += 1
+        return self._nseen == self._ncores
+
+    def process_data(self, testdir, rawiter):
+        # the test passed iff the last line is the finish string
+        nspawned = 0
+        npassed = 0
+        for line in rawiter:
+            if re.match(r'.*pawning .*test_retype2 on core', line):
+                nspawned += 1
+            if line.startswith("retype2: result:"):
+                _,_,r=line.split(':')
+                r = r.strip()
+                if r == "0":
+                    npassed += 1
+        return PassFailResult(npassed == nspawned)
