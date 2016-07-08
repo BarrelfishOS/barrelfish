653914a8a606f0df0f5b6c38be5ceef5b53611a1
Lukas Humbel <lukas.humbel@inf.ethz.ch>
2016-07-08 14:22:30 +0200

    IRQ: Kaluga: put int ctrl startup in own file
    Signed-off-by: Lukas Humbel <lukas.humbel@inf.ethz.ch>

--- usr/kaluga/start_pci.c
+++ usr/kaluga/start_pci.c
@@ -24,11 +24,7 @@
 #include <skb/skb.h>
 #include <thc/thc.h>
 
-#define KALUGA_SERVICE_DEBUG 1
 #include "kaluga.h"
-
-// FOR INT DRIVER
-#include <barrelfish/spawn_client.h>
 
 static void pci_change_event(octopus_mode_t mode, char* device_record, void* st);
 
@@ -70,94 +66,6 @@ static errval_t wait_for_spawnd(coreid_t core, void* state)
     }
 
     return error_code;
-}
-
-/** Turn a line of comma seperated values into a a number of arguments
- * and a pointer array of values (a la argc/argv).
- * string pointed to by in _will be modified_
- * argv will be null terminated
- */
-
-static void csv_to_argv(char * in, int * argc, char *** argv) {
-    // Determine argc
-    *argc = 0;
-    for(char * pos = in; *pos; pos++) *argc += *pos == ',' ? 1 : 0;
-
-    char ** argv_out = (char **)malloc(sizeof(char *) * (*argc) + 1);
-    *argv = argv_out;
-    assert(argv_out != NULL);
-
-    //Replace , with \0 and generate argv
-    int argv_idx = 0;
-    argv_out[argv_idx] = in;
-    argv_idx++;
-    for(char * pos = in; *pos; pos++) {
-        if(*pos == ',') {
-            argv_out[argv_idx] = pos+1;
-            argv_idx++;
-            *pos = '\0';
-        }
-    }
-    argv_out[argv_idx] = NULL;
-}
-
-static void int_controller_change_event(octopus_mode_t mode, char* device_record, void* st)
-{
-    KALUGA_DEBUG("int_controller_change_event!\n");
-
-    errval_t err;
-    char ** argv = NULL;
-
-    if (mode & OCT_ON_SET) {
-        char * label;
-        char * class;
-        KALUGA_DEBUG("Device record: %s\n", device_record);
-        err = oct_read(device_record, "_ { label: %s, class: %s }",
-                &label, &class);
-        if (err_is_fail(err)) {
-            USER_PANIC_ERR(err, "Got malformed int controller device record?");
-            goto out;
-        }
-
-        KALUGA_DEBUG("Int Controller appeared, Label: %s\n", label);
-        // Ask the SKB which binary and where to start it...
-        static char* query = "find_int_controller_driver(%s).";
-        err = skb_execute_query(query, label);
-        if (err_no(err) == SKB_ERR_EXECUTION) {
-            KALUGA_DEBUG("No int controller driver found for: label=%s,class=%s\n",
-                    label, class);
-            goto out;
-        }
-        else if (err_is_fail(err)) {
-            DEBUG_ERR(err, "Failed to query SKB.\n");
-            goto out;
-        }
-
-        char * out = skb_get_output();
-        int argc;
-        csv_to_argv(out, &argc, &argv);
-
-        // TODO: specify core somehow
-        coreid_t core = get_my_core_id();
-        KALUGA_DEBUG("Starting int controller binary: %s\n", argv[0]);
-        err = spawn_program(core, argv[0], argv+1,
-                        environ, 0, NULL);
-        if (err_is_fail(err)) {
-            DEBUG_ERR(err, "Spawning %s failed.", argv[0]);
-        }
-    }
-
-out:
-    free(argv);
-    free(device_record);
-}
-
-errval_t watch_for_int_controller(void) {
-    KALUGA_DEBUG("watch_for_int_controller\n");
-    static char* int_controller_device  = "r'hw\\.int\\.controller\\.[0-9]+' { "
-                               " label: _, class: _ }";
-    octopus_trigger_id_t tid;
-    return oct_trigger_existing_and_watch(int_controller_device, int_controller_change_event, NULL, &tid);
 }
 
 static void pci_change_event(octopus_mode_t mode, char* device_record, void* st)
