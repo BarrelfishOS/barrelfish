fa1904195ee9f42d5387667fb4a889a28e6e3200
Simon Gerber <simon.gerber@inf.ethz.ch>
2016-07-27 14:32:17 +0200

    T266: root cnode resize test: only do a cap_copy() for every 256th slot
    Signed-off-by: Simon Gerber <simon.gerber@inf.ethz.ch>

--- usr/tests/capops/rootcn_resize.c
+++ usr/tests/capops/rootcn_resize.c
@@ -15,14 +15,7 @@
 #include <stdio.h>
 #include <barrelfish/barrelfish.h>
 
-// adapted from usr/monitor/capops/internal.h
-#define GOTO_IF_ERR(err, label) do { \
-    if (err_is_fail(err)) { \
-        printf("...fail: %s\n", err_getstring(err)); \
-        result = 1; \
-        goto label; \
-    } \
-} while (0)
+#define STEP 256
 
 //{{{1 main
 int main(void)
@@ -36,10 +29,12 @@ int main(void)
             printf("test_rootcn_resize: slot alloc %"PRIuCSLOT" failed\n", i);
             return 1;
         }
-        err = cap_copy(cap, cap_vroot);
-        if (err_is_fail(err)) {
-            DEBUG_ERR(err, "cap_copy");
-            printf("test_rootcn_resize: copying cpa to slot %"PRIuCSLOT" failed\n", i);
+        if (i % STEP == 0) {
+            err = cap_copy(cap, cap_vroot);
+            if (err_is_fail(err)) {
+                DEBUG_ERR(err, "cap_copy");
+                printf("test_rootcn_resize: copying cpa to slot %"PRIuCSLOT" failed\n", i);
+            }
         }
     }
     printf("test_rootcn_resize: passed\n");
