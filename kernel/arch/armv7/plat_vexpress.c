7583624ca2268b041fd832d0005ca4d3488a9186
David Cock <david.cock@inf.ethz.ch>
2016-08-29 18:02:51 +0200

    ARMv7: Factored boot protocol out of plat_vexpress
    Signed-off-by: David Cock <david.cock@inf.ethz.ch>

--- kernel/arch/armv7/plat_vexpress.c
+++ kernel/arch/armv7/plat_vexpress.c
@@ -17,7 +17,6 @@
 #include <bitmacros.h>
 #include <a9_gt.h>
 #include <a9_scu.h>
-#include <boot_protocol.h>
 #include <global.h>
 #include <init.h>
 #include <paging_kernel_arch.h>
@@ -71,80 +70,6 @@ void platform_get_info(struct platform_info *pi)
     pi->arch     = PI_ARCH_ARMV7A;
     pi->platform = PI_PLATFORM_VEXPRESS;
     armv7_get_info(&pi->arch_info.armv7);
-}
-
-/*
- * \brief Boot an ARM APP core
- *
- * \param core_id   MPID of the core to try booting
- * \param entry     Entry address for new kernel in the destination
- *                  architecture's lvaddr_t
- *
- * \returns Zero on successful boot, non-zero (error code) on failure
- */
-
-int
-platform_boot_aps(coreid_t core_id, genvaddr_t gen_entry) {
-    assert(paging_mmu_enabled());
-
-    /* XXX - we're abusing the gen_entry pointer here.  Change the interface
-     * to make this arch-specific. */
-    lpaddr_t new_core_data_ptr= (lpaddr_t)gen_entry;
-
-    /* This mailbox is in the boot driver's BSS. */
-    struct armv7_boot_record *br=
-        (struct armv7_boot_record *)core_data->target_bootrecs;
-
-    /* Acquire the lock on the boot record. */
-    spinlock_acquire(&br->lock);
-
-    /* Pass the core data pointer. */
-    br->core_data= new_core_data_ptr;
-
-    /* Clear the completion notification. */
-    br->done= 0;
-
-    /* Flag which core should boot. */
-    /* XXX - this will only work for single-cluster systems, whose MPID fits
-     * entirely within the low 8 bits.  Make core IDs bigger! */
-    br->target_mpid= core_id;
-
-    /* The boot driver will read this value with its MMU and caches disabled,
-     * so we need to make sure it's visible. */
-    dmb(); isb();
-    clean_invalidate_to_poc(&br->core_data);
-    clean_invalidate_to_poc(&br->done);
-    clean_invalidate_to_poc(&br->target_mpid);
-
-    /* We need to ensure that the clean has finished before we wake them. */
-    dmb(); isb();
-
-    /* Wake all sleeping cores. */
-    sev();
-
-    /* The target core will let us know that it's exited the boot driver by
-     * setting done to one *with its MMU, and hence coherency, enabled*. */
-    volatile uint32_t *mailbox= &br->done;
-    while(!*mailbox) wfe();
-
-    /* Release the lock on the boot record. */
-    spinlock_release(&br->lock);
-
-    return 0;
-}
-
-void
-platform_notify_bsp(uint32_t *mailbox) {
-    assert(paging_mmu_enabled());
-
-    /* Set the flag to one (this is br->done, above). */
-    *mailbox= 1;
-
-    /* Make sure that the write has completed. */
-    dmb(); isb();
-
-    /* Wake the booting core. */
-    sev();
 }
 
 uint32_t tsc_hz = 0;
