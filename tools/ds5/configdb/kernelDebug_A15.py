49e865d8a10c71363020856b0e7b3c05dc8fe099
David Cock <david.cock@inf.ethz.ch>
2016-09-02 12:44:51 +0200

    ARMv7: DS-5 script for A15 FVP.
    Signed-off-by: David Cock <david.cock@inf.ethz.ch>

---
+++ tools/ds5/configdb/kernelDebug_A15.py
@@ -0,0 +1,31 @@
+from arm_ds.debugger_v1 import Debugger
+from arm_ds.debugger_v1 import DebugException
+import os
+
+# The CPU driver is linked at this address
+LINKADDRESS = 0
+
+# The boot driver is relocated to this address
+RAMBASE = 0x80000000
+
+debugger = Debugger()
+ec = debugger.getCurrentExecutionContext()
+
+# Load the boot driver symbols
+im= ec.getImageService()
+im.loadSymbols('Barrelfish/armv7/sbin/boot_ve', RAMBASE)
+
+# Get the CPU driver's final load address.
+vs = ec.getVariableService()
+kernel_start = vs.readValue('boot_arguments.cpu_driver_base')
+
+offset = int(kernel_start) - LINKADDRESS
+
+print "Kernel loaded at: %08x" % int(kernel_start), " linked at %08x" % LINKADDRESS, " offset %08x" % offset
+
+# Add CPU driver symbols
+im.addSymbols('Barrelfish/armv7/sbin/cpu_a15ve', offset)
+
+# Finally, advance to arch_init()
+es = ec.getExecutionService()
+es.resumeTo('arch_init')
