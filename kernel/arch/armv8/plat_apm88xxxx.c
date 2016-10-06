058fcd0de70af58dfe17a50b1ee1b18f3e4a5351
Reto Achermann <reto.achermann@inf.ethz.ch>
2016-10-06 10:40:40 +0200

    armv8/apm88xxxx: removing directory and moving functionality to plat_* files.
    Signed-off-by: Reto Achermann <reto.achermann@inf.ethz.ch>

--- kernel/arch/armv8/plat_apm88xxxx.c
+++ kernel/arch/armv8/plat_apm88xxxx.c
@@ -13,4 +13,125 @@
  * ETH Zurich D-INFK, Universitaetsstrasse 6, CH-8092 Zurich. Attn: Systems Group.
  */
 
+errval_t serial_init(unsigned port, bool initialize_hw)
+{
+    if (port >= NUM_PORTS) {
+        return SYS_ERR_SERIAL_PORT_INVALID;
+    }
 
+    if (ports[port].base == (portbases[port] + KERNEL_OFFSET)) {
+        return SYS_ERR_OK;
+    }
+
+    apm88xxxx_pc16550_t *uart = &ports[port];
+    apm88xxxx_pc16550_initialize(uart, (portbases[port] + KERNEL_OFFSET));
+
+    if (!initialize_hw) {
+        // hw initialized, this is for non-bsp cores, where hw has been
+        // initialized by bsp core and we come through here just to setup our
+        // local apm88xxxx_pc16550 struct for the port.
+        return SYS_ERR_OK;
+    }
+
+    panic("device init NYI");
+    return SYS_ERR_OK;
+}
+
+errval_t serial_early_init(unsigned port)
+{
+    if (port >= NUM_PORTS) {
+        return SYS_ERR_SERIAL_PORT_INVALID;
+    }
+
+    if (ports[port].base == portbases[port]) {
+        return SYS_ERR_OK;
+    }
+
+    apm88xxxx_pc16550_t *uart = &ports[port];
+    apm88xxxx_pc16550_initialize(uart, portbases[port]);
+    return SYS_ERR_OK;
+}
+
+
+/**
+ * \brief Prints a single character to a serial port.
+ */
+void serial_putchar(unsigned port, char c)
+{
+    assert(port < NUM_PORTS);
+    assert(ports[port].base != 0);
+    // Wait until FIFO can hold more characters
+    while(!apm88xxxx_pc16550_LSR_thre_rdf(&ports[port]));
+    // Write character
+    apm88xxxx_pc16550_THR_thr_wrf(&ports[port], c);
+}
+
+/**
+ * \brief Reads a single character from the default serial port.
+ * This function spins waiting for a character to arrive.
+ */
+char serial_getchar(unsigned port)
+{
+    assert(port < NUM_PORTS);
+    assert(ports[port].base != 0);
+
+    // Wait until character available
+    while(!apm88xxxx_pc16550_LSR_dr_rdf(&ports[port]));
+    // Read a character from FIFO
+    return apm88xxxx_pc16550_RBR_rbr_rdf(&ports[port]);
+}
+
+
+/*
+ * Timers
+ */
+void     timers_init(int timeslice)
+{
+    uint64_t CNTKCTL_EL1;
+     __asm volatile("mrs %[CNTKCTL_EL1], CNTKCTL_EL1" : [CNTKCTL_EL1] "=r" (CNTKCTL_EL1));
+     CNTKCTL_EL1 |= (1 << 9); // Dont trap access to CNTP_* to EL1
+     CNTKCTL_EL1 |= (1 << 8); // Dont trap access to CNTV_* to EL1
+     CNTKCTL_EL1 |= (1 << 1); // Dont trap access to CNTFRQ* to EL1
+     CNTKCTL_EL1 |= (1 << 0); // Dont trap access to CNTFRQ* to EL1
+     __asm volatile("msr CNTKCTL_EL1, %[CNTKCTL_EL1]" : : [CNTKCTL_EL1] "r" (CNTKCTL_EL1));
+
+
+     uint32_t PMCR_EL0  = 0;
+
+     PMCR_EL0 |= (1 << 0); /* All counters are enabled.*/
+     PMCR_EL0 |= (1 << 1); /* reset all event counters */
+     PMCR_EL0 |= (1 << 2); /* reset all clock counters */
+     PMCR_EL0 |= (0 << 3); /* set counter to tick every clock cycle (1=ever 64th) */
+     PMCR_EL0 |= (1 << 4); /* enable event support */
+     PMCR_EL0 |= (0 << 5); /* don't disable cycle counte r*/
+
+     PMCR_EL0 |= (6 << 11); /* Six counters */
+
+     __asm volatile("msr PMCR_EL0, %[PMCR_EL0]" : : [PMCR_EL0] "r" (PMCR_EL0));
+
+
+     uint32_t PMUSERENR_EL0  = 0;
+
+     PMUSERENR_EL0 |= (1 << 0);  /* don't trap access to PM registers to EL 1 */
+     PMUSERENR_EL0 |= (1 << 1);  /* don't trap software increment wrap to EL 1 */
+     PMUSERENR_EL0 |= (1 << 2);  /* don't trap cycle counter to EL 1 */
+     PMUSERENR_EL0 |= (1 << 3);  /* don't trap event counter read to EL 1*/
+
+     __asm volatile("msr PMUSERENR_EL0, %[PMUSERENR_EL0]" : : [PMUSERENR_EL0] "r" (PMUSERENR_EL0));
+}
+
+uint64_t timestamp_read(void)
+{
+
+    return 0;
+}
+
+uint32_t timestamp_freq(void)
+{
+    return 1;
+}
+
+bool     timer_interrupt(uint32_t irq)
+{
+    return 0;
+}
---
+++ kernel/arch/armv8/plat_apm88xxxx_consts.c
@@ -0,0 +1,60 @@
+/**
+ * \file plat_arm_vm_consts.c
+ * \brief 
+ */
+
+
+/*
+ * Copyright (c) 2016 ETH Zurich.
+ * All rights reserved.
+ *
+ * This file is distributed under the terms in the attached LICENSE file.
+ * If you do not find this file, copies can be found by writing to:
+ * ETH Zurich D-INFK, Universitaetsstrasse 6, CH-8092 Zurich. Attn: Systems Group.
+ */
+
+#include <kernel.h>
+
+/* RAM starts at 0, provided by the MMAP */
+lpaddr_t phys_memory_start= 0;
+
+/*
+ * ----------------------------------------------------------------------------
+ * GIC
+ * ----------------------------------------------------------------------------
+ */
+
+lpaddr_t platform_gic_cpu_base = 0x78000000;
+lpaddr_t platform_gic_dist_base = 0x78090000;
+// 0x00000000780a0000
+
+/*
+ * ----------------------------------------------------------------------------
+ * UART
+ * ----------------------------------------------------------------------------
+ */
+
+/* the maximum number of UARTS supported */
+#define MAX_NUM_UARTS 4
+
+/* the serial console port */
+unsigned int serial_console_port = 0;
+
+/* the debug console port */
+unsigned int serial_debug_port = 0;
+
+/* the number of physical ports */
+unsigned serial_num_physical_ports = 4;
+
+/* uart bases */
+const lpaddr_t
+uart_base[MAX_NUM_UARTS]= {
+        0x1C020000, 0x1C021000, 0x1C02200, 0x1C023000
+        //0x402020000 ?
+};
+
+/* uart sizes */
+const size_t
+uart_size[MAX_NUM_UARTS]= {
+    4096, 4096, 4096, 4096
+};
