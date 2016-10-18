54c6295f0a1237110e29f935dff3c36ff0b7d8be
Lukas Humbel <lukas.humbel@inf.ethz.ch>
2016-10-18 17:53:16 +0200

    Added missing e1000 device id
    Signed-off-by: Lukas Humbel <lukas.humbel@inf.ethz.ch>

--- usr/skb/programs/device_db.pl
+++ usr/skb/programs/device_db.pl
@@ -49,6 +49,7 @@ pci_driver{
       pci_card{ vendor: 16'8086, device: 16'10c9, function: _, subvendor: _, subdevice: _ },
       pci_card{ vendor: 16'8086, device: 16'10a7, function: _, subvendor: _, subdevice: _ },
       pci_card{ vendor: 16'8086, device: 16'10d3, function: _, subvendor: _, subdevice: _ },
+      pci_card{ vendor: 16'8086, device: 16'1079, function: _, subvendor: _, subdevice: _ },
       pci_card{ vendor: 16'8086, device: 16'1533, function: _, subvendor: _, subdevice: _ } ],
     core_hint: 0,
     core_offset: 0,
