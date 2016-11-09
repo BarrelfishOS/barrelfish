a474860d12f34b9069a565339361983e1780736f
Moritz Hoffmann <moritz.hoffmann@inf.ethz.ch>
2016-11-09 16:54:47 +0100

    barrelfish_kpi/platform: Add APM88xxxx, remove TMAS
    Signed-off-by: Moritz Hoffmann <moritz.hoffmann@inf.ethz.ch>

--- include/barrelfish_kpi/platform.h
+++ include/barrelfish_kpi/platform.h
@@ -34,7 +34,7 @@ enum pi_platform {
     PI_PLATFORM_VEXPRESS,
     PI_PLATFORM_ZYNQ7,
     // armv8 platforms
-    PI_PLATFORM_TMAS,
+    PI_PLATFORM_APM88XXXX,
 };
 
 /* Must be at least as large as all architectures' structs. */
