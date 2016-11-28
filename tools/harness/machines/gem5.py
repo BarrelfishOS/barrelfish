9b78e132f4530a6598162ab436719002832917f4
Moritz Hoffmann <moritz.hoffmann@inf.ethz.ch>
2016-11-28 09:20:20 +0100

    harness: Update gem5 tests
    Signed-off-by: Moritz Hoffmann <moritz.hoffmann@inf.ethz.ch>

--- tools/harness/machines/gem5.py
+++ tools/harness/machines/gem5.py
@@ -13,7 +13,7 @@
 
 import os, signal, tempfile, subprocess, shutil, time
 import debug, machines
-from machines import ARMSimulatorBase, MachineFactory
+from machines import ARMSimulatorBase, MachineFactory, ARMSimulatorOperations
 
 GEM5_PATH = '/home/netos/tools/gem5/gem5-stable-1604'
 # gem5 takes quite a while to come up. If we return right away,
@@ -27,16 +27,8 @@ GEM5_START_TIMEOUT = 1 # in seconds
 class Gem5MachineBase(ARMSimulatorBase):
     imagename = "armv7_a15ve_gem5_image"
 
-    def __init__(self, options):
-        super(Gem5MachineBase, self).__init__(options)
-        self.child = None
-        self.telnet = None
-        self.tftp_dir = None
-        self.options = options
-        self.simulator_start_timeout = GEM5_START_TIMEOUT
-        # menu.lst template for gem5 is special
-        # XXX: current template does not work because gem5 coreboot NYI
-        self.menulst_template = "menu.lst.armv7_a15ve_gem5"
+    def __init__(self, options, operations, **kwargs):
+        super(Gem5MachineBase, self).__init__(options, operations, **kwargs)
 
     def get_buildall_target(self):
         return "VExpressEMM-A15"
@@ -49,6 +41,15 @@ class Gem5MachineBase(ARMSimulatorBase):
         # give gem5 tests enough time to complete: skb initialization takes
         # about 10 minutes, so set timeout to 25 minutes.
         return 25 * 60
+
+class Gem5MachineBaseOperations(ARMSimulatorOperations):
+
+    def __init__(self, machine):
+        super(Gem5MachineBaseOperations, self).__init__(machine)
+        self.simulator_start_timeout = GEM5_START_TIMEOUT
+        # menu.lst template for gem5 is special
+        # XXX: current template does not work because gem5 coreboot NYI
+        self.menulst_template = "menu.lst.armv7_a15ve_gem5"
 
     def get_tftp_dir(self):
         if self.tftp_dir is None:
@@ -71,23 +72,29 @@ class Gem5MachineBase(ARMSimulatorBase):
         time.sleep(GEM5_START_TIMEOUT)
 
 class Gem5MachineARM(Gem5MachineBase):
+
+    def __init__(self, options, operations, **kwargs):
+        super(Gem5MachineARM, self).__init__(options, operations, **kwargs)
+
     def get_bootarch(self):
         return 'armv7'
 
     def get_platform(self):
         return 'a15ve'
 
+class Gem5MachineARMOperations(Gem5MachineBaseOperations):
+
     def set_bootmodules(self, modules):
         # write menu.lst in build directory
         debug.verbose("writing menu.lst in build directory")
-        menulst_fullpath = os.path.join(self.options.builds[0].build_dir,
+        menulst_fullpath = os.path.join(self._machine.options.builds[0].build_dir,
                 "platforms", "arm", self.menulst_template)
         debug.verbose("writing menu.lst in build directory: %s" %
                 menulst_fullpath)
-        self._write_menu_lst(modules.get_menu_data("/"), menulst_fullpath)
+        self._machine._write_menu_lst(modules.get_menu_data("/"), menulst_fullpath)
         debug.verbose("building proper gem5 image")
-        debug.checkcmd(["make", self.imagename],
-                cwd=self.options.builds[0].build_dir)
+        debug.checkcmd(["make", self._machine.imagename],
+                cwd=self._machine.options.builds[0].build_dir)
 
 
 # SK: did not test this yet, but should work
@@ -107,6 +114,9 @@ class Gem5MachineARM(Gem5MachineBase):
 class Gem5MachineARMSingleCore(Gem5MachineARM):
     name = 'armv7_gem5'
 
+    def __init__(self, options, **kwargs):
+        super(Gem5MachineARMSingleCore, self).__init__(options, Gem5MachineARMSingleCoreOperations(self), **kwargs)
+
     def get_bootarch(self):
         return "armv7"
 
@@ -119,12 +129,14 @@ class Gem5MachineARMSingleCore(Gem5MachineARM):
     def get_cores_per_socket(self):
         return 1
 
+class Gem5MachineARMSingleCoreOperations(Gem5MachineARMOperations):
+
     def _get_cmdline(self):
         self.get_free_port()
         script_path = \
-            os.path.join(self.options.sourcedir, 'tools/arm_gem5',
+            os.path.join(self._machine.options.sourcedir, 'tools/arm_gem5',
                          'boot_gem5.sh')
-        return ([script_path, 'VExpress_EMM', self.kernel_img, GEM5_PATH,
+        return ([script_path, 'VExpress_EMM', self._machine.kernel_img, GEM5_PATH,
                  str(self.telnet_port)])
 
 MachineFactory.addMachine(Gem5MachineARMSingleCore.name, Gem5MachineARMSingleCore)
