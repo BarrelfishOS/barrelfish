75f7db5df6da0db1bdba09d9310fc55819e1fec3
Moritz Hoffmann <moritz.hoffmann@inf.ethz.ch>
2016-11-28 11:46:11 +0100

    harness: Use self._name instead of self.name
    Shouldn't access private variable anyhow...
    
    Signed-off-by: Moritz Hoffmann <moritz.hoffmann@inf.ethz.ch>

--- tools/harness/machines/qemu.py
+++ tools/harness/machines/qemu.py
@@ -36,7 +36,7 @@ class QEMUMachineBase(Machine):
         return 60
 
     def get_machine_name(self):
-        return self.name
+        return self._name
 
     def get_serial_binary(self):
         return "serial_pc16550d"
