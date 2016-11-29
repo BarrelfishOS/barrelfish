a58cf32091562afc37e1b989305717bf4444d599
Moritz Hoffmann <moritz.hoffmann@inf.ethz.ch>
2016-11-29 19:24:37 +0100

    barrelfish_kpi: Add unknown_arch.h to x86_64 although it is empty
    Signed-off-by: Moritz Hoffmann <moritz.hoffmann@inf.ethz.ch>

---
+++ include/arch/x86_64/barrelfish_kpi/unknown_arch.h
@@ -0,0 +1,17 @@
+/**
+ * \file Exists for compatibility with ARM
+ */
+
+/*
+ * Copyright (c) 2016, ETH Zurich.
+ * All rights reserved.
+ *
+ * This file is distributed under the terms in the attached LICENSE file.
+ * If you do not find this file, copies can be found by writing to:
+ * ETH Zurich D-INFK, Universitaetstr. 6, CH-8092 Zurich. Attn: Systems Group.
+ */
+
+#ifndef ARCH_X86_64_BARRELFISH_KPI_UNKNOWN_H
+#define ARCH_X86_64_BARRELFISH_KPI_UNKNOWN_H
+
+#endif // ARCH_X86_64_BARRELFISH_KPI_UNKNOWN_H
