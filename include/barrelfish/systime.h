a8829f7ca36bc06d15252e22a1d92e9840f39b34
Moritz Hoffmann <moritz.hoffmann@inf.ethz.ch>
2016-11-29 14:45:28 +0100

    barrelfish/systime.h: Need to include kpi/unknown_arch for rdtsc on arm
    Signed-off-by: Moritz Hoffmann <moritz.hoffmann@inf.ethz.ch>

--- include/barrelfish/systime.h
+++ include/barrelfish/systime.h
@@ -17,6 +17,7 @@
 #define BARRELFISH_SYSTIME_H
 
 #include <barrelfish_kpi/types.h> /* systime_t */
+#include <barrelfish_kpi/unknown_arch.h>
 
 /// Frequency of the system time ticks (systime)
 extern systime_t systime_frequency;
