8b4662b2ea230e8d9f43a4c706d0ff618fc7bca2
Kosyrev Serge <_deepfire@feelingofgreen.ru>
2016-12-19 13:38:02 +0300

    x86 | debug:  qemu_debug_puts()
    Signed-off-by: Kosyrev Serge <skosyrev@ptsecurity.com>

--- kernel/include/arch/x86/debug.h
+++ kernel/include/arch/x86/debug.h
@@ -24,4 +24,25 @@ void debug_setup_stackwalker (uint64_t stack_top, uint64_t stack_bottom, uint64_
 void debug_relocate_dynsyms (struct Elf64_Sym *dynsyms, int n, uint64_t offset);
 void dump_stack (void);
 
+/* Citing the QEMU manpage, circa 2016:
+ * -debugcon dev
+ *
+ *   Redirect the debug console to host device dev (same devices as the serial
+ *   port).  The debug console is an I/O port which is typically port 0xe9;
+ *   writing to that I/O port sends output to this device.  The default device is
+ *   "vc" in graphical mode and "stdio" in non graphical mode.
+ */
+static inline void __attribute__ ((always_inline)) qemu_debug_outb (char c)
+{
+    int qemu_debug_port = 0xe9;
+    __asm__ __volatile__("outb %0,%%dx" : : "a" (c), "d" (qemu_debug_port));
+}
+
+static inline void __attribute__ ((always_inline)) qemu_debug_puts (char *s)
+{
+    char c; int i;
+    for (i = 0; (c = s[i]); i++)
+	qemu_debug_outb (c);
+}
+
 #endif //KERNEL_DEBUG_H
