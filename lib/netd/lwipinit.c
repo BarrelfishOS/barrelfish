4c258208643fda3213f340fc84a4b7c9c5a90e8e
Adam Turowski <adam.turowski@inf.ethz.ch>
2017-01-16 09:08:18 +0100

    netd: moving to a library
    Signed-off-by: Adam Turowski <adam.turowski@inf.ethz.ch>

--- lib/netd/lwipinit.c
+++ lib/netd/lwipinit.c
@@ -22,28 +22,14 @@
 #include <lwip/tcp.h>
 #include <netif/bfeth.h>
 
-// keeping it for DHCP timers
-#include <barrelfish/deferred.h>
-
-#include "netd_debug.h"
-#include "netd.h"
+#include "netd_private.h"
+#include <netd/netd_debug.h>
 
 /* Enable tracing only when it is globally enabled */
 #if CONFIG_TRACE && NETWORK_STACK_TRACE
 #define ENABLE_WEB_TRACING 1
 #endif // CONFIG_TRACE && NETWORK_STACK_TRACE
 
-
-static struct periodic_event dhcp_fine_timer; // fine-grain timer for DHCP
-static struct periodic_event dhcp_coarse_timer; // coarse-grain timer for DHCP
-
-// local ip address.  Used to keep track of changing IP addresses
-static struct ip_addr local_ip = {
-    .addr = 0x00,       // BFDMUX_IP_ADDR_ANY
-};
-
-// state variable indicating if dhcp is done or not
-static bool dhcp_completed = false;
 
 static void timer_callback(void *data)
 {
@@ -57,7 +43,7 @@ static void timer_callback(void *data)
 }
 
 
-static void setup_dhcp_timer(void)
+static void setup_dhcp_timer(struct netd_state *state)
 {
     errval_t err;
 
@@ -65,13 +51,13 @@ static void setup_dhcp_timer(void)
     NETD_DEBUG("setting up timeouts for lwip\n");
 
     /* DHCP fine timer */
-    err = periodic_event_create(&dhcp_fine_timer, get_default_waitset(),
+    err = periodic_event_create(&state->dhcp_fine_timer, get_default_waitset(),
                                 (DHCP_FINE_TIMER_MSECS * 1000),
                                 MKCLOSURE(timer_callback, dhcp_fine_tmr));
     assert(err_is_ok(err));
 
     /* DHCP coarse timer */
-    err = periodic_event_create(&dhcp_coarse_timer, get_default_waitset(),
+    err = periodic_event_create(&state->dhcp_coarse_timer, get_default_waitset(),
                                 (DHCP_COARSE_TIMER_MSECS * 1000),
                                 MKCLOSURE(timer_callback, dhcp_coarse_tmr));
     assert(err_is_ok(err));
@@ -79,17 +65,16 @@ static void setup_dhcp_timer(void)
 
 static void link_status_change(struct netif *nf)
 {
-    static bool subsequent_call;
-
+    struct netd_state *state = nf->state;
+    
     if (netif_is_up(nf)) {
         printf("netd: interface is now up\n");
     } else {
         printf("netd: interface is now down\n");
         return;
     }
-
-    if (subsequent_call) {
-        if (ip_addr_cmp(&local_ip, &nf->ip_addr) != 0) {
+    if (state->subsequent_call) {
+        if (ip_addr_cmp(&state->local_ip, &nf->ip_addr) != 0) {
             printf
               ("netd: WARNING: IP has changed! Current address: %d.%d.%d.%d",
                ip4_addr1(&nf->ip_addr), ip4_addr2(&nf->ip_addr),
@@ -117,10 +102,10 @@ static void link_status_change(struct netif *nf)
 
     } // end else:
 
-    local_ip = nf->ip_addr;
+    state->local_ip = nf->ip_addr;
     netif_set_default(nf);
 
-    if (!subsequent_call) {
+    if (!state->subsequent_call) {
 #if 0
         /* Now, the timers are not needed.  They should be closed. */
         /* I don't agree -- we need to keep renewing our lease -AB */
@@ -128,22 +113,22 @@ static void link_status_change(struct netif *nf)
         periodic_event_cancel(&dhcp_coarse_timer);
 #endif // 0
 
-        if (do_dhcp) {
-            dhcp_completed = true;
+        if (state->do_dhcp) {
+            state->dhcp_completed = true;
         }
     } // end if: first call
 
-    subsequent_call = true;
+    state->subsequent_call = true;
 }
 
 
 
-static void get_ip_from_dhcp(struct netif *nf_ptr)
+static void get_ip_from_dhcp(struct netd_state *state, struct netif *nf_ptr)
 {
 
 
     NETD_DEBUG("get_ip_from_dhcp: starting dhcp\n");
-    setup_dhcp_timer();
+    setup_dhcp_timer(state);
     err_t err = dhcp_start(nf_ptr);
 
     assert(err == ERR_OK);
@@ -156,22 +141,26 @@ static void convert_str_to_ip_addr(char *addr_str,
     assert(addr_str != NULL);
     struct in_addr addr;
     if (inet_aton(addr_str, &addr) == 0) {
-        printf("Invalid IP addr: %s\n", ip_addr_str);
-        USER_PANIC("Invalid IP address %s", ip_addr_str);
+        printf("Invalid IP addr: %s\n", addr_str);
+        USER_PANIC("Invalid IP address %s", addr_str);
         return;
     }
     ip_addr_holder->addr = addr.s_addr;
 } // end function: convert_str_to_ip_addr
 
-void startlwip(char *card_name, uint64_t queueid)
+void startlwip(struct netd_state *state, char *card_name, uint64_t queueid)
 {
 
     NETD_DEBUG("NETD is taking control of the LWIP for card[%s][%"PRIu64"]\n",
             card_name, queueid);
+    state->subsequent_call = false;
+    state->local_ip.addr = 0x00; // BFDMUX_IP_ADDR_ANY
+    
     // take ownership of lwip
-    netif_ptr = owner_lwip_init(card_name, queueid);
-    assert(netif_ptr != NULL);
-    netif_set_status_callback(netif_ptr, link_status_change);
+    state->netif_ptr = owner_lwip_init(card_name, queueid);
+    assert(state->netif_ptr != NULL);
+    state->netif_ptr->state = state;
+    netif_set_status_callback(state->netif_ptr, link_status_change);
 
 #if 0
 #if ENABLE_WEB_TRACING
@@ -194,12 +183,12 @@ void startlwip(char *card_name, uint64_t queueid)
         printf("Tracing not enabled\n");
 #endif // ENABLE_WEB_TRACING
 #endif // 0
-    if (do_dhcp) {
-        get_ip_from_dhcp(netif_ptr);
+    if (state->do_dhcp) {
+        get_ip_from_dhcp(state, state->netif_ptr);
 
         NETD_DEBUG("Waiting for DHCP to complete\n");
         struct waitset *ws = get_default_waitset();
-        while (!dhcp_completed) {
+        while (!state->dhcp_completed) {
             errval_t err = event_dispatch(ws);
             if (err_is_fail(err)) {
                 DEBUG_ERR(err, "in event_dispatch (waiting for dhcp)");
@@ -212,18 +201,17 @@ void startlwip(char *card_name, uint64_t queueid)
     // end else: static ip configuration
     // directly set IP address
     printf("Configuring interface with static values\n");
-    printf("ip[%s], nm[%s], gw[%s]\n", ip_addr_str, netmask_str,
-                gateway_str);
+    printf("ip[%s], nm[%s], gw[%s]\n", state->ip_addr_str, state->netmask_str,
+                state->gateway_str);
     struct ip_addr ipaddr, gw, netmask;
-    convert_str_to_ip_addr(ip_addr_str, &ipaddr);
-    convert_str_to_ip_addr(netmask_str, &netmask);
-    convert_str_to_ip_addr(gateway_str, &gw);
+    convert_str_to_ip_addr(state->ip_addr_str, &ipaddr);
+    convert_str_to_ip_addr(state->netmask_str, &netmask);
+    convert_str_to_ip_addr(state->gateway_str, &gw);
 
 
-    netif_set_ipaddr(netif_ptr, &ipaddr);
-    netif_set_gw(netif_ptr, &gw);
-    netif_set_netmask(netif_ptr, &netmask);
-    netif_set_up(netif_ptr);
+    netif_set_ipaddr(state->netif_ptr, &ipaddr);
+    netif_set_gw(state->netif_ptr, &gw);
+    netif_set_netmask(state->netif_ptr, &netmask);
+    netif_set_up(state->netif_ptr);
 
 } // end function startlwip
-
