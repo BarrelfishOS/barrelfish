ca8049ea631f20d007f807cc224b61202a4d4033
Reto Achermann <reto.achermann@inf.ethz.ch>
2017-02-26 16:36:55 +0100

    skb: adding static platform facts for the ThunderX and X-Gene SoC
    Signed-off-by: Reto Achermann <reto.achermann@inf.ethz.ch>

---
+++ usr/skb/programs/plat_apm88xxxx.pl
@@ -0,0 +1,14 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Copyright (c) 2017, ETH Zurich.
+% All rights reserved.
+%
+% This file is distributed under the terms in the attached LICENSE file.
+% If you do not find this file, copies can be found by writing to:
+% ETH Zurich D-INFK, Haldeneggsteig 4, CH-8092 Zurich. Attn: Systems Group.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% Statically-initialised hardware facts for the X-GENE APM88xxxx SoC
+
+cpu_driver("/armv8/sbin/cpu_apm88xxxx").
+monitor("/armv8/sbin/monitor").
+
