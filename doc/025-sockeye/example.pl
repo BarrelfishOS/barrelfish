8357559ff814f4a563d9df3043047fe16f48c7d1
Daniel Schwyn <schwyda@student.ethz.ch>
2017-06-27 12:59:44 +0200

    Technote: Compile Sockeye examples as part of build process
    Signed-off-by: Daniel Schwyn <schwyda@student.ethz.ch>

--- doc/025-sockeye/example.pl
+++
@@ -1,5 +0,0 @@
-net('device1',node(device,[block(16'0,16'fff)],[],'@none')).
-net('device2',node(device,[block(16'0,16'fff)],[],'@none')).
-net('RAM',node(memory,[block(16'0,16'ffff)],[],'@none')).
-net('Interconnect',node(other,[],[map(block(16'0,16'fff),'device1',16'0),map(block(16'2000,16'2fff),'device2',16'0),map(block(16'10000,16'1ffff),'RAM',16'0)],'@none')).
-net('CPU',node(other,[],[],'Interconnect')).
