bf3f507901ea0db1e80d17016e0aceb7be7ac26f
Simon Gerber <simon.gerber@inf.ethz.ch>
2017-10-16 18:43:25 +0200

    mdbbench: update for SG thesis plotting
    Signed-off-by: Simon Gerber <simon.gerber@inf.ethz.ch>

--- tools/harness/tests/mdbbench.py
+++ tools/harness/tests/mdbbench.py
@@ -12,26 +12,33 @@ class MdbBench(TestCommon):
 
     #resets = ["random_nat_ram", "propszrand_nat_ram", "szprob_cp_nat_ram"]
     resets = ["szprob_cp_nat_ram"]
-    counts = [1<<x for x in range(5, 13+1)]
+    counts = [1<<x for x in range(12, 16+1)]
+    #counts = [ 4096 ]
     #counts = [1<<10]
-    impls = ["mdb_bench", "mdb_bench_old"]
+    impls = ["mdb_bench_noparent", "mdb_bench_linkedlist"]
     measures = [
         "insert_one",
         "remove_one",
         "iterate_1",
         "iterate_10",
         "iterate_100",
-        "iterate_1000",
+        #"iterate_1000",
         "has_copies",
         "has_ancestors",
         "has_descendants",
     ]
     impl_measures = {
-        "mdb_bench": [
+        "mdb_bench_noparent": [
             "query_address"
         ],
     }
-    dump = True
+    dump = False
+
+    def __init__(self, options):
+        super(MdbBench, self).__init__(options)
+        import random
+        # is this good enough?
+        self.random_seed = random.randint(0, 1<<32)
 
     def get_build_targets(self, build, machine):
         targets = super(MdbBench, self).get_build_targets(build, machine)
@@ -43,7 +50,8 @@ class MdbBench(TestCommon):
         debug.log('running %s m=%s c=%s' % (impl, measure, count))
         yield "[mdb_bench of %s]\n" % impl
         modules = self.get_modules(build, machine)
-        bench_args = ["count=%s"%count, "reset=%s"%reset, "runs=%d"%runs, "measure=%s"%measure]
+        bench_args = ["core=3", "count=%s"%count, "reset=%s"%reset,
+                "runs=%d"%runs, "measure=%s"%measure, "seed=751993388"]
         modules.add_module(impl, bench_args)
         self.boot(machine, modules)
         self.set_timeout(datetime.timedelta(hours=1))
@@ -81,18 +89,17 @@ class MdbBench(TestCommon):
             m = re.match(r"\[mdb_bench dumping\]", line)
             if m:
                 dumping = True
-            if self.boot_phase:
-                continue
             m = re.match(r"([^:/]+)/(\d+):dump:([^:]+): 0x([0-9a-fA-F]+)/(\d+) ([c.][a.][d.])", line)
             if m:
                 caps.add_row([m.group(3), m.group(1), m.group(2), m.group(4), m.group(5), m.group(6)])
                 continue
-            m = re.match(r"([^:]+):([^:]+): (\d+)/(\d+)", line)
+            m = re.match(r"([^:]+):([^:]+):([^:]+): (\d+)/(\d+)", line)
             if m:
-                reset = m.group(1)
-                measure = m.group(2)
-                count = m.group(4)
-                ticks = m.group(3)
+                assert(impl == m.group(1))
+                reset = m.group(2)
+                measure = m.group(3)
+                count = m.group(5)
+                ticks = m.group(4)
                 results.add_row([impl, reset, measure, count, ticks])
                 continue
 
