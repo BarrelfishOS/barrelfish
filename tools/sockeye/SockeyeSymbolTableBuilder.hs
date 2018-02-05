34ccea8bfba722ba03f21ba46fc69e3105a68680
Daniel Schwyn <schwyda@student.ethz.ch>
2018-02-05 16:50:27 +0100

    Sockeye: Build symbol table with imports
    Signed-off-by: Daniel Schwyn <schwyda@student.ethz.ch>

--- tools/sockeye/SockeyeSymbolTableBuilder.hs
+++ tools/sockeye/SockeyeSymbolTableBuilder.hs
@@ -19,6 +19,8 @@
 module SockeyeSymbolTableBuilder
     (buildSymbolTable) where
 
+import Data.List (intercalate)
+
 import Data.Map (Map)
 import qualified Data.Map as Map
 
@@ -28,12 +30,17 @@ import qualified Data.Set as Set
 import SockeyeChecks
 
 import SockeyeASTMeta
-import SockeyeParserAST as AST
-import SockeyeSymbolTable as ST
+import qualified SockeyeParserAST as AST
+import qualified SockeyeSymbolTable as ST
 
 data SymbolTableFail
-    = DuplicateModule String ASTMeta
+    = CircularImports [FilePath]
+    | DuplicateImport String ASTMeta
+    | NoSuchExport String String
+    | ImportShadowing String String String ASTMeta
+    | DuplicateModule String ASTMeta
     | DuplicateType   String ASTMeta
+    | ModuleTypeClash String ASTMeta ASTMeta
     | DuplicateParameter String
     | DuplicateConstant String ASTMeta
     | ParameterShadowing String ASTMeta
@@ -41,13 +48,27 @@ data SymbolTableFail
     | DuplicateNode String ASTMeta
 
 instance CheckFailure SymbolTableFail where
-    errorLines (DuplicateModule name firstDefined)    = ["Duplicate module '" ++ name ++ "'", "First defined at " ++ show firstDefined]
-    errorLines (DuplicateType   name firstDefined)    = ["Duplicate type '" ++ name ++ "'", "First defined at " ++ show firstDefined]
-    errorLines (DuplicateParameter name)              = ["Duplicate parameter '" ++ name ++ "'"]
-    errorLines (DuplicateConstant name firstDefined)  = ["Duplicate named constant '" ++ name ++ "'", "First defined at " ++ show firstDefined]
-    errorLines (ParameterShadowing name firstDefined) = ["Named constant '" ++ name ++ "' shadows module parameter", "Parameter is declared at " ++ show firstDefined]
-    errorLines (DuplicateInstance name firstDefined)  = ["Duplicate instance '" ++ name ++ "'", "First defined at " ++ show firstDefined]
-    errorLines (DuplicateNode name firstDefined)      = ["Duplicate node '" ++ name ++ "'", "First defined at " ++ show firstDefined]
+    errorLines (CircularImports loop)                                    = ["Circular imports detected:", intercalate " -> " $ reverse loop]
+    errorLines (DuplicateImport name firstDefined)                       = ["Duplicate import '" ++ name ++ "'", "First imported at " ++ show firstDefined]
+    errorLines (NoSuchExport name file)                                  = ["File '" ++ file ++ "' does not export symbol '" ++ name ++ "'"]
+    errorLines (ImportShadowing name defSymType impSymType firstDefined) = [defSymType ++ " '" ++ name ++ "' shadows imported " ++ impSymType, "Imported at " ++ show firstDefined]
+    errorLines (DuplicateModule name firstDefined)                       = ["Duplicate module '" ++ name ++ "'", "First defined at " ++ show firstDefined]
+    errorLines (DuplicateType   name firstDefined)                       = ["Duplicate type '" ++ name ++ "'", "First defined at " ++ show firstDefined]
+    errorLines (ModuleTypeClash name mDef tDef)                          = ["Module and type with same name '" ++ name ++ "'", "Module declared at " ++ show mDef, "Type declared at " ++ show tDef]
+    errorLines (DuplicateParameter name)                                 = ["Duplicate parameter '" ++ name ++ "'"]
+    errorLines (DuplicateConstant name firstDefined)                     = ["Duplicate named constant '" ++ name ++ "'", "First defined at " ++ show firstDefined]
+    errorLines (ParameterShadowing name firstDefined)                    = ["Named constant '" ++ name ++ "' shadows module parameter", "Parameter is declared at " ++ show firstDefined]
+    errorLines (DuplicateInstance name firstDefined)                     = ["Duplicate instance '" ++ name ++ "'", "First defined at " ++ show firstDefined]
+    errorLines (DuplicateNode name firstDefined)                         = ["Duplicate node '" ++ name ++ "'", "First defined at " ++ show firstDefined]
+
+data Import
+    = TypeImport ST.NamedType
+    | ModuleImport ST.Module
+    deriving (Show)
+
+instance MetaAST Import where
+    meta (TypeImport t) = meta t
+    meta (ModuleImport m) = meta m
 
 buildSymbolTable :: AST.Sockeye -> Either (FailedChecks SymbolTableFail) ST.Sockeye
 buildSymbolTable ast = runChecks $ symbol ast
@@ -58,24 +79,147 @@ class SymbolSource a b where
 instance SymbolSource AST.Sockeye ST.Sockeye where
     symbol ast = do
         let entryPoint = AST.entryPoint ast
-        entryFile <- symbol $ (AST.files ast) Map.! entryPoint
+        files <- fileSymbols [] entryPoint Map.empty
         return ST.Sockeye
             { ST.entryPoint = entryPoint
-            , ST.files      = Map.singleton entryPoint entryFile
+            , ST.files      = files
             }
+        where
+            fileSymbols predecessors filePath fileSyms
+               | filePath `Map.member` fileSyms = return fileSyms
+               | otherwise = do
+                    let file = (AST.files ast) Map.! filePath
+                        imports = AST.imports file
+                    fileSyms' <- foldChecks importFileSymbols fileSyms imports
+                    fileSym <- symbol file
+                    importSymbols <- foldChecks (collectImports fileSyms') Map.empty imports
+                    fileSym' <- foldChecks addImportSymbol fileSym $ Map.assocs importSymbols
+                    return $ Map.insert filePath fileSym' fileSyms'
+                where
+                    importFileSymbols importAst fileSyms = do
+                        let importFile = AST.importFile importAst
+                        case loop importFile predecessors of
+                            [] -> fileSymbols (importFile:predecessors) importFile fileSyms
+                            loop -> failCheck (meta importAst) (CircularImports $ importFile:loop) >> return fileSyms
+                    loop name [] = []
+                    loop name path@(p:ps)
+                        | name `elem` path = p:(loop name ps)
+                        | otherwise = []
+
+collectImports :: Map FilePath ST.SockeyeFile -> AST.Import -> Map String Import -> Checks SymbolTableFail (Map String Import)
+collectImports fileSymbols importAst importSymbols = do
+    let importedFileName = AST.importFile importAst
+        importedFile = fileSymbols Map.! importedFileName
+    case AST.explImports importAst of
+        Just as -> foldChecks importWithAlias importSymbols as
+        Nothing -> do
+            withTypes <- foldChecks implicitTypeImport importSymbols (Map.assocs $ ST.types importedFile)
+            foldChecks implicitModuleImport withTypes (Map.assocs $ ST.modules importedFile)
+    where
+        importWithAlias a importSymbols = do
+            let importedFileName = AST.importFile importAst
+                importedFile = fileSymbols Map.! importedFileName
+                origName = AST.originalName a
+                types = ST.types importedFile
+                modules = ST.modules importedFile
+            {-
+             - Import either type or module
+             - Only one can exist (see ModuleTypeClash check)
+             -}
+            case Map.lookup origName types of
+                Just t -> explicitTypeImport a t importSymbols
+                Nothing -> case Map.lookup origName modules of
+                    Just m -> explicitModuleImport a m importSymbols
+                    Nothing -> do
+                        failCheck (meta a) $ NoSuchExport origName importedFileName
+                        return importSymbols
+        {- Imported types are not exported -}
+        explicitTypeImport a (ST.ImportedType {}) importSymbols = do
+            failCheck (meta importAst) $ NoSuchExport (AST.originalName a) (AST.importFile importAst)
+            return importSymbols
+        explicitTypeImport a (ST.NamedType {}) importSymbols = do
+            let alias = AST.importAlias a
+            case Map.lookup alias importSymbols of
+                Nothing -> return $ Map.insert alias (importedType (AST.originalName a) $ meta a) importSymbols
+                Just dup -> do
+                    failCheck (meta a) $ DuplicateImport alias (meta dup)
+                    return importSymbols
+        {- Imported modules are not exported -}
+        explicitModuleImport a (ST.ImportedModule {}) importSymbols = do
+            failCheck (meta a) $ NoSuchExport (AST.originalName a) (AST.importFile importAst)
+            return importSymbols
+        explicitModuleImport a (ST.Module {}) importSymbols = do
+            let alias = AST.importAlias a
+            case Map.lookup alias importSymbols of
+                Nothing -> return $ Map.insert alias (importedModule (AST.originalName a) $ meta a) importSymbols
+                Just dup -> do
+                    failCheck (meta a) $ DuplicateImport alias (meta dup)
+                    return importSymbols
+        {- Only import declared types, not imported ones -}
+        implicitTypeImport (_, ST.ImportedType {}) importSymbols = return importSymbols
+        implicitTypeImport (name, ST.NamedType {}) importSymbols = case Map.lookup name importSymbols of
+            Nothing -> return $ Map.insert name (importedType name $ meta importAst) importSymbols
+            Just dup -> do
+                failCheck (meta importAst) $ DuplicateImport name (meta dup)
+                return importSymbols
+        {- Only import declared modules, not imported ones -}
+        implicitModuleImport (_, ST.ImportedModule {}) importSymbols = return importSymbols
+        implicitModuleImport (name, ST.Module {}     ) importSymbols = case Map.lookup name importSymbols of
+            Nothing -> return $ Map.insert name (importedModule name $ meta importAst) importSymbols
+            Just dup -> do
+                failCheck (meta importAst) $ DuplicateImport name (meta dup)
+                return importSymbols
+        importedType typeName m = TypeImport $ ST.ImportedType
+            { ST.namedTypeMeta = m
+            , ST.typeFile      = AST.importFile importAst
+            , ST.origTypeName  = typeName
+            }
+        importedModule modName m = ModuleImport $ ST.ImportedModule
+            { ST.moduleMeta  = m
+            , ST.moduleFile  = AST.importFile importAst
+            , ST.origModName = modName
+            }
+
+addImportSymbol :: (String, Import) -> ST.SockeyeFile -> Checks SymbolTableFail ST.SockeyeFile
+addImportSymbol (name, TypeImport i) fileSymbol = do
+    case Map.lookup name (ST.modules fileSymbol) of
+        Nothing -> return ()
+        Just m -> failCheck (meta m) $ ImportShadowing name "Module" "type" (meta i)
+    let ts = ST.types fileSymbol
+    case Map.lookup name ts of
+        Nothing -> return $ fileSymbol { ST.types = Map.insert name i ts }
+        Just t -> failCheck (meta t) (ImportShadowing name "Type" "type" (meta i)) >> return fileSymbol
+addImportSymbol (name, ModuleImport i) fileSymbol = do
+    case Map.lookup name (ST.types fileSymbol) of
+        Nothing -> return ()
+        Just t -> failCheck (meta t) $ ImportShadowing name "Type" "module" (meta i)
+    let ms = ST.modules fileSymbol
+    case Map.lookup name ms of
+        Nothing -> return $ fileSymbol { ST.modules = Map.insert name i ms }
+        Just m -> failCheck (meta m) (ImportShadowing name "Module" "module" (meta i)) >> return fileSymbol
+                    
 
 instance SymbolSource AST.SockeyeFile ST.SockeyeFile where
     symbol ast = do
         modules <- symbolMap AST.moduleName moduleDupFail $ AST.modules ast
         types <- symbolMap AST.typeName typeDupFail $ AST.types ast
+        types' <- foldChecks checkModTypeClash types $ Map.assocs modules
         return ST.SockeyeFile
             { ST.sockeyeFileMeta = AST.sockeyeFileMeta ast
             , ST.modules = modules
-            , ST.types = types
+            , ST.types = types'
             }
         where
             moduleDupFail m s = DuplicateModule (AST.moduleName m) (meta s)
             typeDupFail t s = DuplicateType (AST.typeName t) (meta s)
+            checkModTypeClash (name, m) ts = case Map.lookup name ts of
+                Nothing -> return ts
+                Just t -> do
+                    let mMeta = meta m
+                        tMeta = meta t
+                    failCheck (max mMeta tMeta) $ ModuleTypeClash name mMeta tMeta
+                    return $ Map.delete name ts
+
 
 instance SymbolSource AST.Module ST.Module where
     symbol ast = do
