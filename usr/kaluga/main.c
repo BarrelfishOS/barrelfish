671f74578affe3379caa76c177796afc45a529c7
Lukas Humbel <lukas.humbel@inf.ethz.ch>
2018-02-28 11:03:16 +0100

    Fix kaluga compilation on armv7
    Signed-off-by: Lukas Humbel <lukas.humbel@inf.ethz.ch>

--- usr/kaluga/main.c
+++ usr/kaluga/main.c
@@ -48,12 +48,16 @@ static void add_start_function_overrides(void)
 {
 
     set_start_function("e10k", start_networking);
-    set_start_function("sfn5122f", start_networking_new);
-    set_start_function("e1000n", start_networking_new);
     set_start_function("net_sockets_server", start_networking);
-    set_start_function("e1000n_irqtest", default_start_function_new);
     set_start_function("rtl8029", start_networking);
     set_start_function("corectrl", start_boot_driver);
+
+#ifndef __ARM_ARCH_7A__
+    set_start_function("sfn5122f", start_networking_new);
+    set_start_function("e1000n", start_networking_new);
+    set_start_function("e1000n_irqtest", default_start_function_new);
+#endif
+
 #ifdef __ARM_ARCH_7A__
     set_start_function("driverdomain", newstyle_start_function);
 #endif
