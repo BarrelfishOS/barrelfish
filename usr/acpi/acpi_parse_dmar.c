f7b8e7b8ea841dcb9c24a69d7fe8d1a4baea3e6c
Reto Achermann <reto.achermann@inf.ethz.ch>
2018-02-28 17:18:52 +0100

    acpi: fixing bugs when parsing dmar table
    Signed-off-by: Reto Achermann <reto.achermann@inf.ethz.ch>

--- usr/acpi/acpi_parse_dmar.c
+++ usr/acpi/acpi_parse_dmar.c
@@ -27,28 +27,6 @@
 #include "acpi_shared.h"
 
 
-/*
- * local_apic(proc_id:8, apicid:8, flags:32);
- * io_apic(apic_id:8, pbaseaddress:32, globalirqbase:32)
- * interrupt_override(bus:8, sourceirq:8, globalirq:32, intiflags:16)
- * nmi_source(intiflags:16, globalirq:32)
- * local_apic_nmi(proc_id:8, intiflags:16, lint:8)
- * local_apic_overritde(address:64)
- * io_sapic(id:8, globalirqbase:32, address:64)
- * local_sapic(proc_id:8, id:8, eid:8, lapicflags:32, uid:32, uidstring:x)
- * interrupt_source(intiflags:16, type:8, id:8, eid:8, iosapicvector:8, globalirq:32, flags:32)
- * local_x2apic(localapicid:32, laicflags:32,uid:32)
- * local_x2apic_nmi(intiflags:16, uid:32, lint:8)
- * generic_interrupt(
-     CpuInterfaceNumber:32; Uid:32, Flags:32, ParkingVersion:32,
-    PerformanceInterrupt:32, ParkedAddress:64, BaseAddress:64,
-    GicvBaseAddress:64, GichBaseAddress:64, VgicInterrupt:32, GicrBaseAddress:64,
-    ArmMpidr:64, EfficiencyClass:8)
- * generic_distributor(gicid:32, baseaddr:64, globalirqbase:32, version:8)
- * generic_msi_frame(msiframeid:32, baseaddr:64, flags:32, spicount:16, spibase:16)
- * generic_redistributor(baseaddr: 64, length:32)
- * generic_translator(translationid:32, baseaddr:64)
- */
 
 #define SKB_SCHEMA_DMAR \
     "dmar(%" PRIu8 ")."
@@ -81,27 +59,36 @@ static errval_t parse_device_scope(ACPI_DMAR_DEVICE_SCOPE *dsc, void *end,
     errval_t err;
     ACPI_DMAR_PCI_PATH *pcip;
 
-    debug_printf("[dmar] [drhd]\n");
-
     while((void *)dsc < end) {
-        assert(dsc->Length - sizeof(ACPI_DMAR_DEVICE_SCOPE) == 2);
+        debug_printf("[dmar] [dscp] parsing device scope. length %u, segment %u, "
+                             "memory [%p..%p]\n",
+                     dsc->Length, segment, dsc, end);
+
         pcip = (ACPI_DMAR_PCI_PATH *)((uint8_t *)dsc + sizeof(ACPI_DMAR_DEVICE_SCOPE));
         void *pcip_end = ((uint8_t *) dsc + dsc->Length);
         while((void *)pcip < pcip_end) {
+            uint64_t bus = 0xffff;
+
+            debug_printf("[dmar] [dscp] parsing PCI path of PCI Addr=[%u.%u.%u]\n",
+                         dsc->Bus, pcip->Function, pcip->Device);
+
+            #if 0
             err = skb_execute_query("bridge(PCIE,addr(%d,%d,%d),_,_,_,_,_,secondary(BUS)),"
                                     "write(secondary_bus(BUS)).", dsc->Bus,
                                     pcip->Device, pcip->Function);
             if (err_is_fail(err)) {
+                DEBUG_ERR(err, "[dmar] [dscp] SKB query failed\n");
                 goto loop_next_pcip;
             }
 
-            uint64_t bus;
+
             err = skb_read_output("secondary_bus(%" SCNu64 ")", &bus);
             if (err_is_fail(err)) {
+                DEBUG_ERR(err, "[dmar] [dscp] reading query result\n");
                 goto loop_next_pcip;
             }
-
-            debug_printf("[dmar] [drhd]" SKB_SCHEMA_DMAR_DEVSC "\n",
+            #endif
+            debug_printf("[dmar] [dscp] " SKB_SCHEMA_DMAR_DEVSC "\n",
                          type, dsc->EntryType, segment, (uint8_t)bus, pcip->Device,
                          pcip->Function, dsc->EnumerationId);
 
@@ -114,7 +101,7 @@ static errval_t parse_device_scope(ACPI_DMAR_DEVICE_SCOPE *dsc, void *end,
                           segment, (uint8_t)bus, pcip->Device, pcip->Function,
                           dsc->EnumerationId);
             }
-loop_next_pcip:
+//loop_next_pcip:
             pcip += 1;
         }
 
@@ -129,7 +116,7 @@ static errval_t parse_hardware_unit(ACPI_DMAR_HARDWARE_UNIT *drhd, void *end)
 {
     errval_t err;
 
-    debug_printf("[dmar] [drhd]" SKB_SCHEMA_DMAR_HW_UNIT "\n",
+    debug_printf("[dmar] [drhd] " SKB_SCHEMA_DMAR_HW_UNIT "\n",
                  drhd->Flags, drhd->Segment, drhd->Address);
 
     err = skb_add_fact(SKB_SCHEMA_DMAR_HW_UNIT, drhd->Flags, drhd->Segment,
@@ -139,13 +126,15 @@ static errval_t parse_hardware_unit(ACPI_DMAR_HARDWARE_UNIT *drhd, void *end)
                   drhd->Flags, drhd->Segment, drhd->Address);
     }
 
-    void *sub = ((uint8_t *)drhd) + sizeof(ACPI_DMAR_ATSR);
+    void *sub = ((uint8_t *)drhd) + sizeof(ACPI_DMAR_HARDWARE_UNIT);
     err = parse_device_scope(sub, end, drhd->Segment, ACPI_DMAR_TYPE_HARDWARE_UNIT);
     if (err_is_fail(err)) {
         DEBUG_ERR(err, "Failed to parse device scope: " SKB_SCHEMA_DMAR_HW_UNIT "\n",
                   drhd->Flags, drhd->Segment, drhd->Address);
     }
 
+    debug_printf("[dmar] [drhd] set " HW_PCI_IOMMU_RECORD_FORMAT "\n",
+                 HW_PCI_IOMMU_INTEL, drhd->Flags, drhd->Segment, drhd->Address);
     return oct_mset(SET_SEQUENTIAL, HW_PCI_IOMMU_RECORD_FORMAT, HW_PCI_IOMMU_INTEL,
                     drhd->Flags, drhd->Segment, drhd->Address);
 }
@@ -154,7 +143,7 @@ static errval_t parse_reserved_memory(ACPI_DMAR_RESERVED_MEMORY *rmem, void *end
 {
     errval_t err;
 
-    debug_printf("[dmar] [rmem]" SKB_SCHEMA_DMAR_RESERVED_MEMORY "\n",
+    debug_printf("[dmar] [rmem] " SKB_SCHEMA_DMAR_RESERVED_MEMORY "\n",
                  rmem->Segment, rmem->BaseAddress, rmem->EndAddress);
 
     err = skb_add_fact(SKB_SCHEMA_DMAR_RESERVED_MEMORY,
@@ -165,7 +154,7 @@ static errval_t parse_reserved_memory(ACPI_DMAR_RESERVED_MEMORY *rmem, void *end
                   rmem->Segment, rmem->BaseAddress, rmem->EndAddress);
     }
 
-    void *sub = ((uint8_t *)rmem) + sizeof(ACPI_DMAR_ATSR);
+    void *sub = ((uint8_t *)rmem) + sizeof(ACPI_DMAR_RESERVED_MEMORY);
     return parse_device_scope(sub, end, rmem->Segment,
                               ACPI_DMAR_TYPE_RESERVED_MEMORY);
 }
@@ -174,7 +163,7 @@ static errval_t parse_root_ats_capabilities(ACPI_DMAR_ATSR *atsr, void *end)
 {
     errval_t err;
 
-    debug_printf("[dmar] [atsr]" SKB_SCHEMA_DMAR_ATSR "\n",
+    debug_printf("[dmar] [atsr] " SKB_SCHEMA_DMAR_ATSR "\n",
                  atsr->Flags, atsr->Segment);
     err = skb_add_fact(SKB_SCHEMA_DMAR_ATSR, atsr->Flags, atsr->Segment);
     if (err_is_fail(err)) {
@@ -192,7 +181,7 @@ static errval_t parse_root_ats_capabilities(ACPI_DMAR_ATSR *atsr, void *end)
 
 static errval_t parse_hardware_resource_affinity(ACPI_DMAR_RHSA *rhsa)
 {
-    debug_printf("[dmar] [rhsa]" SKB_SCHEMA_DMAR_RHSA "\n",
+    debug_printf("[dmar] [rhsa] " SKB_SCHEMA_DMAR_RHSA "\n",
                  rhsa->BaseAddress, rhsa->ProximityDomain);
 
     return skb_add_fact(SKB_SCHEMA_DMAR_RHSA, rhsa->BaseAddress,
@@ -201,7 +190,7 @@ static errval_t parse_hardware_resource_affinity(ACPI_DMAR_RHSA *rhsa)
 
 static errval_t parse_namespace_device_declaration(ACPI_DMAR_ANDD *andd)
 {
-    debug_printf("[dmar] [andd]" SKB_SCHEMA_DMAR_ANDD "\n",
+    debug_printf("[dmar] [andd] " SKB_SCHEMA_DMAR_ANDD "\n",
                  andd->DeviceNumber, andd->DeviceName);
 
     return skb_add_fact(SKB_SCHEMA_DMAR_ANDD, andd->DeviceNumber,
@@ -222,13 +211,17 @@ errval_t acpi_parse_dmar(void)
 
     if(ACPI_FAILURE(as)) {
         debug_printf("No DMAR found in ACPI! Cannot initialize IO MMUs.\n");
+
+        return oct_mset(SET_SEQUENTIAL, HW_PCI_IOMMU_RECORD_FORMAT, HW_PCI_IOMMU_INTEL,
+                        0, 0, 0);
+
         return ACPI_ERR_OBJECT_NOT_FOUND;
     }
     else {
         dmar = (ACPI_TABLE_DMAR*)ath;
     }
 
-    skb_add_fact("dmar(%"PRIu8")", dmar->Flags);
+    skb_add_fact(SKB_SCHEMA_DMAR, dmar->Flags);
 
 
     debug_printf("DMAR Revision: %u, Size=%u, OEM=%s\n", dmar->Header.Revision,
@@ -246,19 +239,34 @@ errval_t acpi_parse_dmar(void)
 
         switch (sh->Type) {
             case ACPI_DMAR_TYPE_HARDWARE_UNIT:
-                parse_hardware_unit(p, p_end);
+                err = parse_hardware_unit(p, p_end);
+                if (err_is_fail(err)) {
+                    DEBUG_ERR(err, "parsing hardware unit failed\n");
+                }
                 break;
             case ACPI_DMAR_TYPE_RESERVED_MEMORY:
-                parse_reserved_memory(p, p_end);
+                err = parse_reserved_memory(p, p_end);
+                if (err_is_fail(err)) {
+                    DEBUG_ERR(err, "reserved memory failed\n");
+                }
                 break;
             case ACPI_DMAR_TYPE_ROOT_ATS:
-                parse_root_ats_capabilities(p, p_end);
+                err = parse_root_ats_capabilities(p, p_end);
+                if (err_is_fail(err)) {
+                    DEBUG_ERR(err, "parsing root ats caps failed\n");
+                }
                 break;
             case ACPI_DMAR_TYPE_HARDWARE_AFFINITY:
                 err = parse_hardware_resource_affinity(p);
+                if (err_is_fail(err)) {
+                    DEBUG_ERR(err, "parsing resource affinity failed\n");
+                }
                 break;
             case ACPI_DMAR_TYPE_NAMESPACE:
                 err = parse_namespace_device_declaration(p);
+                if (err_is_fail(err)) {
+                    DEBUG_ERR(err, "parsing namespace declarations failed\n");
+                }
                 break;
             default:
                 assert(!"Reserved for future use!\n");
