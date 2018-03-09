cf03abef0e36aee8acd76a8d4d662588726c0f43
Roni Häcki <roni.haecki@inf.ethz.ch>
2018-03-09 13:53:21 +0100

    skb: changed e1000 core hint to core 1
    Signed-off-by: Roni Häcki <roni.haecki@inf.ethz.ch>

--- usr/skb/programs/device_db.pl
+++ usr/skb/programs/device_db.pl
@@ -51,9 +51,10 @@ pci_driver{
       pci_card{ vendor: 16'8086, device: 16'10a7, function: _, subvendor: _, subdevice: _ },
       pci_card{ vendor: 16'8086, device: 16'10d3, function: _, subvendor: _, subdevice: _ },
       pci_card{ vendor: 16'8086, device: 16'1079, function: _, subvendor: _, subdevice: _ },
-      pci_card{ vendor: 16'8086, device: 16'1533, function: _, subvendor: _, subdevice: _ }
+      pci_card{ vendor: 16'8086, device: 16'1533, function: _, subvendor: _, subdevice: _ },
+      pci_card{ vendor: 16'8086, device: 16'15b8, function: _, subvendor: _, subdevice: _ }
     ],
-    core_hint: 0,
+    core_hint: 1,
     core_offset: 0,
     multi_instance: 0,
     interrupt_load: 0.75,
