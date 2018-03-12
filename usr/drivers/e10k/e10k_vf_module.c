a0e2bcec6ed0e9cd574eb24ea5b341e42ea528c6
Roni Häcki <roni.haecki@inf.ethz.ch>
2018-03-12 17:59:17 +0100

    e10k: virtual function module
    Signed-off-by: Roni Häcki <roni.haecki@inf.ethz.ch>

---
+++ usr/drivers/e10k/e10k_vf_module.c
@@ -0,0 +1,117 @@
+/*
+ * Copyright (c) 2007-2011, 2013, 2014, ETH Zurich.
+ * All rights reserved.
+ *
+ * This file is distributed under the terms in the attached LICENSE file.
+ * If you do not find this file, copies can be found by writing to:
+ * ETH Zurich D-INFK, Haldeneggsteig 4, CH-8092 Zurich. Attn: Systems Group.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+
+#include <net_device_manager/net_device_manager.h>
+#include <pci/pci.h>
+#include <barrelfish/nameservice_client.h>
+#include <barrelfish/debug.h>
+#include <skb/skb.h>
+#include <driverkit/driverkit.h>
+
+/**
+ * Driver initialization function. This function is called by the driver domain
+ * (see also 'create_handler' in ddomain_service.c).
+ * Typically through a request from the device manager.
+ *
+ * The init function is supposed to set `dev` to the exported service iref.
+ * The init function may use `bfi->dstate` to store additional state about the device.
+ *
+ * \param[in]   bfi   The instance of this driver.
+ * \param[in]   name  The name of this driver instance.
+ * \param[in]   flags Additional flags (The exact flags supported is device/driver specific).
+ * \param[in]   c     Capabilities (for registers etc.) as provided by the device manager.
+ *                    The exact layout of the `c` is device specific.
+ * \param[out]  dev   The service iref over which the device can be contacted.
+ *
+ * \retval SYS_ERR_OK Device initialized successfully.
+ * \retval LIB_ERR_MALLOC_FAIL Unable to allocate memory for the driver.
+ */
+static errval_t init(struct bfdriver_instance *bfi, uint64_t flags, iref_t *dev)
+{
+    printf("VF driver started\n");
+
+    /*
+    errval_t err;
+    bfi->dstate = calloc(sizeof(struct e10k_driver_state), 1);
+    if (bfi->dstate == NULL) {
+        return LIB_ERR_MALLOC_FAIL;
+    }
+    
+    assert(bfi->dstate != NULL);
+    struct e10k_driver_state* st = (struct e10k_driver_state*) bfi->dstate;
+    st->caps = bfi->caps;
+
+    init_default_values(st);
+    st->bfi = bfi;
+    */
+    return SYS_ERR_OK;
+}
+
+
+/**
+ * Instructs driver to attach to the device.
+ * This function is only called if the driver has previously detached
+ * from the device (see also detach).
+ *
+ * \note After detachment the driver can not assume anything about the
+ * configuration of the device.
+ *
+ * \param[in]   bfi   The instance of this driver.
+ * \retval SYS_ERR_OK Device initialized successfully.
+ */
+static errval_t attach(struct bfdriver_instance* bfi) {
+    return SYS_ERR_OK;
+}
+
+/**
+ * Instructs driver to detach from the device.
+ * The driver must yield any control over to the device after this function returns.
+ * The device may be left in any state.
+ *
+ * \param[in]   bfi   The instance of this driver.
+ * \retval SYS_ERR_OK Device initialized successfully.
+ */
+static errval_t detach(struct bfdriver_instance* bfi) {
+    return SYS_ERR_OK;
+}
+
+/**
+ * Instructs the driver to go in a particular sleep state.
+ * Supported states are platform/device specific.
+ *
+ * \param[in]   bfi   The instance of this driver.
+ * \retval SYS_ERR_OK Device initialized successfully.
+ */
+static errval_t set_sleep_level(struct bfdriver_instance* bfi, uint32_t level) {
+    return SYS_ERR_OK;
+}
+
+/**
+ * Destroys this driver instance. The driver will yield any
+ * control over the device and free any state allocated.
+ *
+ * \param[in]   bfi   The instance of this driver.
+ * \retval SYS_ERR_OK Device initialized successfully.
+ */
+static errval_t destroy(struct bfdriver_instance* bfi) {
+    return SYS_ERR_OK;
+}
+
+/**
+ * Registers the driver module with the system.
+ *
+ * To link this particular module in your driver domain,
+ * add it to the addModules list in the Hakefile.
+ */
+DEFINE_MODULE(e10k_vf_module, init, attach, detach, set_sleep_level, destroy);
